049c3c1b7bdfa06b49718cbc9f9dedde4b5008fd
apparentorder <github@neveragain.de>
2020-12-01 08:57:33 +0100

    re-write as recursive, generalized version

--- 2020/day01.swift
+++ 2020/day01.swift
@@ -1,35 +1,43 @@
+//
+// N.B.: This is version is a bit more cleaned up, generalized and optimized. For the version
+// used to solve at first, see git history.
+//
 class Day01 {
+	static func findSum(_ sum: Int, inArray remaining: [Int], maxDepth: Int, having: [Int]) -> [Int] {
+		let currentSum = having.reduce(0, { $0 + $1 })
 
-	static func findSum(_ sum: Int, ofExpenses expenses: [Int], withCount: Int) -> [Int] {
-		for e1 in expenses {
-			for e2 in expenses {
-				if withCount == 3 {
-					for e3 in expenses {
-						if e1 + e2 + e3 == 2020 {
-							print("MATCH: \(e1) + \(e2) + \(e3)")
-							return [e1, e2, e3]
-						}
-					}
-				} else {
-					if e1 + e2 == 2020 {
-						print("MATCH: \(e1) + \(e2)")
-						return [e1, e2]
-					}
-				}
-			}
+		guard having.count != maxDepth else {
+			// max. depth reached
+			if having.reduce(0, { $0 + $1 }) == 2020 { return having }
+			return []
 		}
 
-		err("no match found")
+		guard currentSum < sum else {
+			// no point in checking further if we already overshot
+			return []
+		}
+
+		guard remaining.count > 0 else { return [] }
+
+		var a = remaining
+		while a.count > 0 {
+			let next = a.removeFirst()
+			let r = findSum(sum, inArray: a, maxDepth: maxDepth, having: having + [next])
+
+			if r.count > 0 { return r }
+		}
+
+		return []
 	}
 
 	static func part1(_ input: PuzzleInput) -> PuzzleResult {
-		let entries = findSum(2020, ofExpenses: input.intArray, withCount: 2)
-		return entries[0] * entries[1]
+		let entries = findSum(2020, inArray: input.intArray, maxDepth: 2, having: [])
+		return entries.reduce(1, { $0 * $1 })
 	}
 
 	static func part2(_ input: PuzzleInput) -> PuzzleResult {
-		let entries = findSum(2020, ofExpenses: input.intArray, withCount: 3)
-		return entries[0] * entries[1] * entries[2]
+		let entries = findSum(2020, inArray: input.intArray, maxDepth: 3, having: [])
+		return entries.reduce(1, { $0 * $1 })
 	}
 }
 
