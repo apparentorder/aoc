c1fa7ddea8bde430d267a0054a8e75738a80ead0
apparentorder <github@neveragain.de>
2020-12-06 07:02:06 +0100

    clean up the garbage that was my first shot at this

--- 2020/day06.swift
+++ 2020/day06.swift
@@ -1,81 +1,43 @@
 class Day06 {
-	static func parseGroups (inputLines: [String]) -> [Int] {
-		var answer = 0
-		var r = [Int]()
+	typealias AnswersByQuestion = Dictionary<Character, Int>
+	struct GroupAnswers {
+		var pax = 0
+		var answers = AnswersByQuestion()
 
-		for line in inputLines {
-			guard !line.isEmpty else {
-				// next group
-				debug("group answer \(answer)")
-				r += [answer]
-				answer = 0
-				continue
+		var sumPart1: Int { answers.keys.count }
+		var sumPart2: Int { answers.filter { $0.1 == pax }.count }
+	}
+
+	static func parsev2(_ input: [[String]]) -> [GroupAnswers] {
+		var r = [GroupAnswers]()
+
+		for lineGroup in input {
+			var ga = GroupAnswers()
+
+			for line in lineGroup {
+				ga.pax += 1
+
+				for question in line {
+					ga.answers[question] = (ga.answers[question] ?? 0) + 1
+				}
 			}
 
-			for question in line {
-				answer |= 1 << (question.asciiValue! - 97)
-			}
+			r += [ga]
 		}
 
-		// last group
-		debug("group answer \(answer)")
-		r += [answer]
-			
 		return r
 	}
 
-	static func parsev2(inputLines: [String]) -> PuzzleResult {
-		var answers = [Character:Int]()
-		var paxInGroup = 0
-		var sum = 0
-
-		for line in inputLines {
-			guard !line.isEmpty else {
-				debug("group answers \(answers) for \(paxInGroup) passengers")
-				for (_, v) in answers {
-					if v == paxInGroup {
-						sum += 1
-					}
-				}
-				answers = [Character:Int]()
-				paxInGroup = 0
-				continue
-			}
-
-			paxInGroup += 1
-			for question in line {
-				answers[question] = (answers[question] ?? 0) + 1
-			}
-		}
-
-		debug("group answers \(answers) for \(paxInGroup) passengers")
-		for (_, v) in answers {
-			if v == paxInGroup {
-				sum += 1
-			}
-		}
-
-		return sum
-	}
-
 	static func part1(_ input: PuzzleInput) -> PuzzleResult {
-		var sum = 0
-
-		for answer in parseGroups(inputLines: input.lines) {
-			debug("\(answer) ...")
-			for i in 0..<26 {
-				if answer & (1 << i) != 0 {
-					debug("... bit \(i)")
-					sum += 1
-				}
-			}
-		}
-
-		return sum
+		return parsev2(input.lineGroups)
+			.map { $0.sumPart1 }
+			.reduce(0, +)
 	}
 
 	static func part2(_ input: PuzzleInput) -> PuzzleResult {
-		return parsev2(inputLines: input.lines)
+		return parsev2(input.lineGroups)
+			.map { $0.sumPart2 }
+			.reduce(0, +)
 	}
 }
 
