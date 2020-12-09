4f76ee889673a984bc9fdec3d61219447089a8b9
apparentorder <github@neveragain.de>
2020-12-09 09:45:11 +0100

    add alternate faster approach for p2 (and another take on p1, but basically same algo, speed diff. is negligible)

--- 2020/day09.swift
+++ 2020/day09.swift
@@ -1,4 +1,55 @@
 class Day09: PuzzleClass {
+	func part1rolling(_ input: PuzzleInput) -> PuzzleResult {
+		let buffer = input.intArray
+		let preambleCount = buffer.count < 25 ? 5 : 25 // hack: preamble of test is 5 instead of 25
+
+		for i in preambleCount..<buffer.count {
+			let num = buffer[i]
+
+			var isValid = false
+			outer: for a in (i - preambleCount)..<i {
+				guard buffer[a] < num else { continue }
+				for b in (i - preambleCount)..<i {
+					if buffer[a] + buffer[b] == num {
+						isValid = true
+						break outer
+					}
+				}
+			}
+
+			guard isValid else { return num }
+		}
+
+		err("nothing found?")
+	}
+
+	func part2rolling(_ input: PuzzleInput) -> PuzzleResult {
+		let buffer = input.intArray
+		let invalidNum = part1rolling(input)
+		var sum = 0
+
+		var start = 0
+
+		debug("wanted: \(invalidNum)")
+		for end in 0..<buffer.count {
+			sum += buffer[end]
+
+			while sum > invalidNum {
+				sum -= buffer[start]
+				start += 1
+			}
+
+			if sum == invalidNum {
+				let rolling = buffer[start...end]
+				return rolling.min()! + rolling.max()!
+			}
+		}
+
+		err("nope")
+	}
+
+	// -----
+
 	func part1(_ input: PuzzleInput) -> PuzzleResult {
 		var buffer = input.intArray
 		let preambleCount = buffer.count < 25 ? 5 : 25 // hack: preamble of test is 5 instead of 25
@@ -58,8 +109,22 @@ class Day09: PuzzleClass {
 				PuzzleTest(PuzzleInput(fromFile: "09-input-test"), result: 127),
 			]
 		),
+		"p1rolling": Puzzle(
+			implementation: part1rolling,
+			input: PuzzleInput(fromFile: "09-input"),
+			tests: [
+				PuzzleTest(PuzzleInput(fromFile: "09-input-test"), result: 127),
+			]
+		),
 		"p2": Puzzle(
 			implementation: part2,
+			input: PuzzleInput(fromFile: "09-input"),
+			tests: [
+				PuzzleTest(PuzzleInput(fromFile: "09-input-test"), result: 62),
+			]
+		),
+		"p2rolling": Puzzle(
+			implementation: part2rolling,
 			input: PuzzleInput(fromFile: "09-input"),
 			tests: [
 				PuzzleTest(PuzzleInput(fromFile: "09-input-test"), result: 62),
