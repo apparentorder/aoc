13b3032aac8b626d475d7b9995a74a5979037a72
apparentorder <github@neveragain.de>
2020-12-10 08:58:42 +0100

    cleanup on aisle 347250213298688!

--- 2020/day10.swift
+++ 2020/day10.swift
@@ -1,120 +1,64 @@
 class Day10: PuzzleClass {
-	func foo(_ intArray: [Int], isPart2: Bool) -> PuzzleResult {
-		var adapters = intArray
+	//
+	// Solving this puzzle relies on the assumption that input numbers are
+	// in groups. Each group is at most five elements and the gap between
+	// each group is exactly two numbers.
+	//
+	// Therefore, the number of different paths *per group* is easily calculated,
+	// and the individual groups' path counts just need to be multiplied.
+	//
 
-		var diff1 = 0
-		var diff2 = 0
-		var diff3 = 0
-		var last = 0
-		//var totalOptions = 1
-
-		while !adapters.isEmpty {
-			debug("\(last) => \(adapters)")
-			//var options = 0
-
-			let containsPlus1 = adapters.contains(last + 1)
-			let containsPlus2 = adapters.contains(last + 2)
-			let containsPlus3 = adapters.contains(last + 3)
-
-			if containsPlus1 {
-				adapters.removeAll(where: { $0 == last + 1 })
-				last += 1
-				diff1 += 1
-				continue
-			}
-
-			if containsPlus2 {
-				adapters.removeAll(where: { $0 == last + 2 })
-				last += 2
-				diff2 += 1
-				continue
-			}
-
-			if containsPlus3 {
-				adapters.removeAll(where: { $0 == last + 3 })
-				last += 3
-				diff3 += 1
-				continue
-			}
-
-			err("no matching adapter")
-		}
-
-		// "your device's built-in adapter is always 3 higher than the highest adapter"
-		diff3 += 1
-
-		//return isPart2 ? totalOptions : diff1 * diff3
-		return isPart2 ? 0 : diff1 * diff3
-	}
-
-	func countPaths(lastUsed: Int, adaptersLeft: [Int], adaptersUsed: [Int]) -> Int {
-		debug("countPaths with lastUsed \(lastUsed)")
-		var paths = 0
-
-		guard !adaptersLeft.isEmpty else {
-			debug("valid path: \(adaptersUsed)")
-			return 1
-		}
-
-		for a in adaptersLeft {
-			//debug("\(a)?")
-			if a >= lastUsed + 1 && a <= lastUsed + 3 {
-				debug("\(a)!")
-				paths += countPaths(
-					lastUsed: a,
-					adaptersLeft: adaptersLeft.filter { $0 != a },
-					adaptersUsed: adaptersUsed + [a]
-				)
-			}
-		}
-
-		return paths
-	}
-
-	func meh(_ input: PuzzleInput) -> Int {
+	func adapterGroups(_ input: PuzzleInput) -> [[Int]] {
 		let adapters = [0] + input.intArray.sorted()
 		debug("\(adapters)")
 
-		var block = [Int]()
-		var blocks = [[Int]]()
-		var paths = 1
+		var group = [Int]()
+		var groups = [[Int]]()
+		var prev = -1
 
 		for a in adapters {
-			if a != (block.max() ?? -1) + 1 {
-				// new block
-				blocks += [block]
-				block = []
+			if a != prev + 1 {
+				// new group
+				groups += [group]
+				group = []
 			}
 
-			block += [a]
+			group += [a]
+			prev = a
 		}
 
 		// flush last
-		blocks += [block]
+		groups += [group]
 
-		for block in blocks {
-			debug("block: \(block)")
-			switch block.count {
+		groups.forEach { debug("group: \($0)") }
+
+		return groups
+	}
+
+	func part1(_ input: PuzzleInput) -> PuzzleResult {
+		let groups = adapterGroups(input)
+		let diff3 = groups.count
+		let diff1 = groups.reduce(0, { $0 + $1.count - 1 })
+		return diff1 * diff3
+	}
+
+	func part2(_ input: PuzzleInput) -> PuzzleResult {
+		let groups = adapterGroups(input)
+		var paths = 1
+
+		for group in groups {
+			debug("group: \(group)")
+			switch group.count {
 			case 1: paths *= 1
 			case 2: paths *= 1
 			case 3: paths *= 2
 			case 4: paths *= 4
 			case 5: paths *= 7
-			default: err("invalid block \(block)")
+			default: err("invalid group \(group)")
 			}
 		}
 
 		return paths
-	}
-
-	func part1(_ input: PuzzleInput) -> PuzzleResult {
-		return foo(input.intArray, isPart2: false)
-	}
-
-	func part2(_ input: PuzzleInput) -> PuzzleResult {
-		//let a = input.intArray
-		//return countPaths(lastUsed: 0, adaptersLeft: a, adaptersUsed: [])
-		return meh(input)
 	}
 
 	// -------------------------------------------------------------
