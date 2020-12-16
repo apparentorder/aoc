68138336bcd1b4abea12c6a945e1f39b89786683
apparentorder <github@neveragain.de>
2020-12-16 10:49:03 +0100

    cleanup

--- 2020/day16.swift
+++ 2020/day16.swift
@@ -1,11 +1,94 @@
 class Day16: PuzzleClass {
-	var rules = [String:(min1: Int, max1: Int, min2: Int, max2: Int)]()
-	var fieldIds = [String:Int]()
+	typealias Constraints = (min1: Int, max1: Int, min2: Int, max2: Int)
+
+	var allRules = [String:Constraints]()
 	var myTicket = [Int]()
 	var otherTickets = [[Int]]()
 
-	func parseTickets(_ inputLines: [String]) {
-		var lines = inputLines
+	func part1(_ input: PuzzleInput) -> PuzzleResult {
+		parseTickets(input)
+
+		return otherTickets
+			.flatMap { invalidFields(inTicket: $0) }
+			.reduce(0, +)
+	}
+
+	func part2(_ input: PuzzleInput) -> PuzzleResult {
+		parseTickets(input)
+
+		otherTickets.removeAll { !invalidFields(inTicket: $0).isEmpty }
+		otherTickets += [myTicket]
+
+		var fieldIds = [String:Int]()
+		var remainingRules = allRules
+		let numberOfFields = otherTickets[0].count
+		var remainingFieldIds = Array(0..<numberOfFields)
+
+		while true {
+			var anyMatches = false
+
+			for fieldId in remainingFieldIds {
+				var validRulesForField = remainingRules
+
+				for ticket in otherTickets {
+					for (ruleName, c) in validRulesForField {
+						if !isValidField(ticket[fieldId], forConstraints: c) {
+							validRulesForField.removeValue(forKey: ruleName)
+							anyMatches = true
+						}
+					}
+				}
+
+				guard !validRulesForField.isEmpty else {
+					err("no rules match for field \(fieldId)")
+				}
+
+				guard validRulesForField.count == 1 else {
+					debug("multiple rules match for field \(fieldId): \(validRulesForField)")
+					continue
+				}
+
+				let matchingRuleName = validRulesForField.keys.first!
+				debug("MATCH: field \(fieldId) is \(matchingRuleName)")
+				fieldIds[matchingRuleName] = fieldId
+				remainingFieldIds.removeAll { $0 == fieldId }
+				remainingRules.removeValue(forKey: matchingRuleName)
+			}
+
+			guard !remainingRules.isEmpty else { break }
+			guard !remainingFieldIds.isEmpty else { break }
+			guard anyMatches else { err("i'm stuck") }
+		}
+
+		guard fieldIds.count == numberOfFields else { err("didn't find all fields") }
+
+		return fieldIds
+			.filter { $0.0.hasPrefix("departure") }
+			.map { myTicket[$0.1] }
+			.reduce(1, *)
+	}
+
+	func isValidField(_ n: Int, forConstraints c: Constraints) -> Bool {
+		return ((n >= c.min1 && n <= c.max1) || (n >= c.min2 && n <= c.max2))
+	}
+
+	func isValidField(_ n: Int) -> Bool {
+		for (_, c) in allRules {
+			if isValidField(n, forConstraints: c) {
+				return true
+			}
+		}
+
+		return false
+	}
+
+	func invalidFields(inTicket ticket: [Int]) -> [Int] {
+		return ticket.filter { !isValidField($0) }
+	}
+
+	func parseTickets(_ input: PuzzleInput) {
+		var lines = input.lines
+
 		while true {
 			let line = lines.removeFirst()
 			guard !line.isEmpty else { break }
@@ -15,96 +98,21 @@ class Day16: PuzzleClass {
 			let rangeComp1 = rangeComp[0].components(separatedBy: "-")
 			let rangeComp2 = rangeComp[1].components(separatedBy: "-")
 
-			debug("\(line) l=\(lineComp), r=\(rangeComp1) r2=\(rangeComp2)")
-			rules[lineComp[0]] =  (Int(rangeComp1[0])!, Int(rangeComp1[1])!, Int(rangeComp2[0])!, Int(rangeComp2[1])!)
+			allRules[lineComp[0]] =  Constraints(
+				min1: Int(rangeComp1[0])!,
+				max1: Int(rangeComp1[1])!,
+				min2: Int(rangeComp2[0])!,
+				max2: Int(rangeComp2[1])!
+			)
 		}
 
-		debug("done; \(rules)")
-
-		lines.removeFirst()
+		lines.removeFirst() // "your ticket:"
 		myTicket = lines.removeFirst().components(separatedBy: ",").map { Int($0)! }
 
-		lines.removeFirst(2)
+		lines.removeFirst(2) // \n, "nearby tickets:"
 		for line in lines {
-			debug("OT: \(line)")
 			otherTickets += [line.components(separatedBy: ",").map { Int($0)! }]
 		}
-
-		debug("my: \(myTicket)")
-		debug("others: \(otherTickets)")
-	}
-
-	func invalidNumbersInTicket(_ ticket: [Int]) -> [Int] {
-		var r = [Int]()
-
-		for number in ticket {
-			var isValid = false
-			for (_, rule) in rules {
-				if (number >= rule.min1 && number <= rule.max1) || (number >= rule.min2 && number <= rule.max2) {
-					isValid = true
-					break
-				}
-			}
-
-			if !isValid {
-				debug("ticket \(ticket) number \(number) is not valid")
-				r += [number]
-			}
-		}
-
-		return r
-	}
-
-	func part1(_ input: PuzzleInput) -> PuzzleResult {
-		parseTickets(input.lines)
-		var r = 0
-
-		for ticket in otherTickets {
-			let invalidNumbers = invalidNumbersInTicket(ticket)
-			invalidNumbers.forEach { r += $0 }
-		}
-
-		return r
-	}
-
-	func part2(_ input: PuzzleInput) -> PuzzleResult {
-		parseTickets(input.lines)
-
-		otherTickets.removeAll { !invalidNumbersInTicket($0).isEmpty }
-		otherTickets += [myTicket]
-
-		var remainingRules = rules
-		var remainingFieldIds = Array(0..<otherTickets[0].count)
-		while true {
-			guard !remainingFieldIds.isEmpty else { break }
-			for fieldId in remainingFieldIds {
-				var validRulesForField = remainingRules
-
-				for ticket in otherTickets {
-					for (ruleName, rule) in validRulesForField {
-						let n = ticket[fieldId]
-						if !((n >= rule.min1 && n <= rule.max1) || (n >= rule.min2 && n <= rule.max2)) {
-							validRulesForField[ruleName] = nil
-						}
-					}
-				}
-
-				guard validRulesForField.count == 1 else {
-					debug("multiple rules match for field \(fieldId): \(validRulesForField)")
-					continue
-				}
-
-				debug("MATCH: field \(fieldId) is \(validRulesForField.keys.first!)")
-				fieldIds[validRulesForField.keys.first!] = fieldId
-				remainingFieldIds.removeAll { $0 == fieldId }
-				remainingRules[validRulesForField.keys.first!] = nil
-			}
-		}
-
-		return fieldIds
-			.filter { $0.0.hasPrefix("departure") }
-			.map { myTicket[$0.1] }
-			.reduce(1, *)
 	}
 
 	// -------------------------------------------------------------
@@ -121,7 +129,8 @@ class Day16: PuzzleClass {
 			implementation: part2,
 			input: PuzzleInput(fromFile: "16-input"),
 			tests: [
-				// nb fixed test data to include departure rules
+				// N.B.: We have manually added "departure" to the "class" and "row" fields
+				// so this test produces a comparable result for part 2.
 				PuzzleTest(PuzzleInput(fromFile: "16-input-test-part2"), result: 132),
 			]
 		),
