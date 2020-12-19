5170e13fa3166849ee2bd2153e34748761985751
apparentorder <github@neveragain.de>
2020-12-19 15:42:28 +0100

    cleanup

--- 2020/day19.swift
+++ 2020/day19.swift
@@ -2,64 +2,6 @@ class Day19: PuzzleClass {
 	var resolvedRules = [Int:[String]](minimumCapacity: 500)
 	var unresolvedRules = [Int:String](minimumCapacity: 500)
 
-	func parseRules(_ input: [String]) {
-		for line in input {
-			let parts = line.components(separatedBy: ": ")
-			let ruleId = Int(parts[0])!
-
-			if !parts[1].hasPrefix("\"") {
-				unresolvedRules[ruleId] = parts[1]
-			} else {
-				// final rule, i.e. already resolved
-				resolvedRules[ruleId] = [parts[1].filter { $0 != "\"" }]
-			}
-		}
-		debug("initially resolved rules: \(resolvedRules)")
-	}
-
-	func resolveRule(_ ruleId: Int, depth: Int = 0) -> [String] {
-		if let validStrings = resolvedRules[ruleId] {
-			return validStrings
-		}
-
-		var validStrings = [String]()
-		//validStrings.reserveCapacity(10_0000)
-
-		debug("START resolving rule \(ruleId)", indent: 4*depth)
-		let subRules = unresolvedRules[ruleId]!.components(separatedBy: " | ")
-		for subRule in subRules {
-			var validStringsForSubRule = [String]()
-			//validStrings.reserveCapacity(10_0000)
-
-			let otherRuleIds = subRule.components(separatedBy: " ").map { Int($0)! }
-			for otherRuleId in otherRuleIds {
-				debug("... resolving rule \(ruleId), other rule \(otherRuleId)", indent: 4*(depth+1))
-				let otherStrings = resolveRule(otherRuleId, depth: depth + 1)
-
-				var newVSFSR = [String]()
-				//newVSFSR.reserveCapacity(10_0000)
-
-				for os in otherStrings {
-					if validStringsForSubRule.isEmpty {
-						newVSFSR += [os]
-					} else {
-						for vsfr in validStringsForSubRule {
-							newVSFSR += [vsfr + os]
-						}
-					}
-				}
-				validStringsForSubRule = newVSFSR
-			}
-
-			validStrings += validStringsForSubRule
-		}
-
-		debug("END resolving rule \(ruleId) to valid strings \(validStrings)", indent: 4*depth)
-
-		resolvedRules[ruleId] = validStrings
-		return validStrings
-	}
-
 	func part1(_ input: PuzzleInput) -> PuzzleResult {
 		let rules = input.lineGroups[0]
 		let messages = input.lineGroups[1]
@@ -103,69 +45,101 @@ class Day19: PuzzleClass {
 		let validStringsRule31 = resolveRule(31)
 		let validStringsRule42 = resolveRule(42)
 
-		debug("xxx 31: \(validStringsRule31)")
-		debug("xxx 42: \(validStringsRule42)")
-
-		debug("xxx 31: min \(validStringsRule31.map { $0.count }.min()!)")
-		debug("xxx 31: max \(validStringsRule31.map { $0.count }.max()!)")
-		debug("xxx 42: min \(validStringsRule42.map { $0.count }.min()!)")
-		debug("xxx 42: max \(validStringsRule42.map { $0.count }.max()!)")
-
-		debug("xxx intersect 31, 42: \(Set(validStringsRule31).intersection(validStringsRule42))")
+		debug("rule 31: valid strings: \(validStringsRule31)")
+		debug("rule 42: valid strings: \(validStringsRule42)")
 
 		var validCount = 0
 		for message in messages {
 			var remainingMessage = message
-			var prefixesMatched = [String]()
-			var suffixesMatched = [String]()
 
-			// remove any matching suffix
-			while true {
-				var suffixMatched = false
-				validStringsRule31.forEach {
-					if remainingMessage.hasSuffix($0) {
-						suffixesMatched.insert($0, at: 0)
-						remainingMessage.removeLast($0.count)
-						suffixMatched = true
-						return
-					}
-				}
-				guard suffixMatched else { break }
-			}
+			let suffixesMatched = trim(anyOf: validStringsRule31, from: &remainingMessage, fromStart: false)
+			guard suffixesMatched > 0 else { continue }
 
-			// bail out if no suffixes matched
-			guard remainingMessage.count != message.count else {
-				continue
-			}
+			let prefixesMatched = trim(anyOf: validStringsRule42, from: &remainingMessage, fromStart: true)
+			guard prefixesMatched > suffixesMatched else { continue }
 
-			// bail out if no potential prefixes are left
-			guard !remainingMessage.isEmpty else { continue }
-
-			// remove any matching prefix
-			while true {
-				var prefixMatched = false
-				validStringsRule42.forEach {
-					if remainingMessage.hasPrefix($0) {
-						prefixesMatched += [$0]
-						remainingMessage.removeFirst($0.count)
-						prefixMatched = true
-						return
-					}
-				}
-				guard prefixMatched else { break }
-			}
-
-			// message must be empty now, as we were looking for ^(42)+(31)+$
-			guard remainingMessage.isEmpty else { continue }
-
-			// need more prefixes than suffixes
-			guard prefixesMatched.count > suffixesMatched.count else { continue }
+			guard remainingMessage.isEmpty else { continue } // message must have no other parts in the middle
 
 			validCount += 1
-			debug("valid message: \(message) => \(prefixesMatched) ... \(suffixesMatched)")
+			debug("valid message: \(message)")
 		}
 
 		return validCount
+	}
+
+	func parseRules(_ input: [String]) {
+		for line in input {
+			let parts = line.components(separatedBy: ": ")
+			let ruleId = Int(parts[0])!
+
+			if !parts[1].hasPrefix("\"") {
+				unresolvedRules[ruleId] = parts[1]
+			} else {
+				// final rule, i.e. already resolved
+				resolvedRules[ruleId] = [parts[1].filter { $0 != "\"" }]
+			}
+		}
+		debug("initially resolved rules: \(resolvedRules)")
+	}
+
+	func resolveRule(_ ruleId: Int, depth: Int = 0) -> [String] {
+		if let validStrings = resolvedRules[ruleId] {
+			return validStrings
+		}
+
+		var validStrings = [String]()
+
+		debug("START resolving rule \(ruleId)", indent: 4*depth)
+		let subRules = unresolvedRules[ruleId]!.components(separatedBy: " | ")
+		for subRule in subRules {
+			var validStringsForSubRule = [String]()
+
+			let otherRuleIds = subRule.components(separatedBy: " ").map { Int($0)! }
+			for otherRuleId in otherRuleIds {
+				debug("... resolving rule \(ruleId), other rule \(otherRuleId)", indent: 4*(depth+1))
+				let otherStrings = resolveRule(otherRuleId, depth: depth + 1)
+
+				guard !validStringsForSubRule.isEmpty else {
+					validStringsForSubRule = otherStrings
+					continue
+				}
+
+				var newVSFSR = [String]()
+				for os in otherStrings {
+					for vsfr in validStringsForSubRule {
+						newVSFSR += [vsfr + os]
+					}
+				}
+				validStringsForSubRule = newVSFSR
+			}
+
+			validStrings += validStringsForSubRule
+		}
+
+		debug("END resolving rule \(ruleId) to valid strings \(validStrings)", indent: 4*depth)
+
+		resolvedRules[ruleId] = validStrings
+		return validStrings
+	}
+
+	func trim(anyOf matches: [String], from string: inout String, fromStart: Bool) -> Int {
+		var matchCount = 0
+		outer: while true {
+			for match in matches {
+				if fromStart && string.hasPrefix(match) {
+					string.removeFirst(match.count)
+					matchCount += 1
+					continue outer
+				} else if !fromStart && string.hasSuffix(match) {
+					string.removeLast(match.count)
+					matchCount += 1
+					continue outer
+				}
+			}
+			break
+		}
+
+		return matchCount
 	}
 
 	// -------------------------------------------------------------
