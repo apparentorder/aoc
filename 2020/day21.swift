9623b3674b6c664146e538d4afe9fbe5e0fec491
apparentorder <github@neveragain.de>
2020-12-21 08:02:32 +0100

    cleanup

--- 2020/day21.swift
+++ 2020/day21.swift
@@ -5,6 +5,55 @@ class Day21: PuzzleClass {
 	}
 
 	var allFoods = [Food]()
+	var allergenIngredient = [String:String]()
+
+	func filterAllergens() {
+		var allAllergens = Set(allFoods.flatMap { $0.allergens })
+
+		while !allAllergens.isEmpty {
+			debug("NEXT ITERATION; allAllergens = \(allAllergens.sorted())")
+
+			for allergen in allAllergens {
+				var ingredients = Set<String>()
+
+				for food in allFoods where food.allergens.contains(allergen) {
+					ingredients = (ingredients.isEmpty) ?
+						Set(food.ingredients) :
+						ingredients.intersection(food.ingredients)
+
+					debug("for \(allergen): remaining possible matches: \(ingredients)")
+
+					guard ingredients.count != 1 else {
+						let match = ingredients.first!
+						debug("exact match: \(match) contains \(allergen)")
+
+						allAllergens.remove(allergen)
+						allergenIngredient[allergen] = match
+
+						for i in 0..<allFoods.count {
+							allFoods[i].ingredients.removeAll { $0 == match }
+						}
+
+						break
+					}
+				}
+			}
+		}
+	}
+
+	func part1(_ input: PuzzleInput) -> PuzzleResult {
+		parse(input)
+		filterAllergens()
+		let remainingIngredients = allFoods.flatMap { $0.ingredients }
+		debug("remaining: \(remainingIngredients)")
+		return remainingIngredients.count
+	}
+
+	func part2(_ input: PuzzleInput) -> PuzzleResult {
+		parse(input)
+		filterAllergens()
+		return allergenIngredient.sorted(by: { $0.key < $1.key }).map { $0.value }.joined(separator: ",")
+	}
 
 	func parse(_ input: PuzzleInput) {
 		for line in input.lines {
@@ -16,57 +65,6 @@ class Day21: PuzzleClass {
 
 			allFoods += [Food(ingredients: ingredients, allergens: allergens)]
 		}
-	}
-
-	func part1(_ input: PuzzleInput) -> PuzzleResult {
-		parse(input)
-
-		var allAllergens = Set(allFoods.flatMap { $0.allergens })
-		var ingredientByAllergen = [String:String]() // part2
-
-		while true {
-			debug("NEXT ITERATION; allAllergens = \(allAllergens.sorted())")
-			guard !allAllergens.isEmpty else { break }
-
-			for findAllergen in allAllergens {
-				var possibleMatches = Set<String>()
-
-				for food in allFoods where food.allergens.contains(findAllergen) {
-					if possibleMatches.isEmpty {
-						possibleMatches.formUnion(food.ingredients)
-						debug("for \(findAllergen): possible matches set to \(possibleMatches)")
-					} else {
-						debug("for \(findAllergen): possible matches \(possibleMatches) intersect \(food.ingredients)")
-						possibleMatches.formIntersection(food.ingredients)
-					}
-				}
-
-				guard possibleMatches.count != 1 else {
-					let match = possibleMatches.first!
-					debug("exact match: \(match) contains \(findAllergen)")
-
-					allAllergens.remove(findAllergen)
-					ingredientByAllergen[findAllergen] = match
-
-					for i in 0..<allFoods.count {
-						allFoods[i].ingredients.removeAll { $0 == match }
-					}
-
-					continue
-				}
-			}
-		}
-
-		let remainingIngredients = allFoods.flatMap { $0.ingredients }
-		debug("remaining: \(remainingIngredients)")
-
-		print("PART 2: \(ingredientByAllergen.sorted(by: { $0.key < $1.key }).map { $0.value }.joined(separator: ","))")
-
-		return remainingIngredients.count
-	}
-
-	func part2(_ input: PuzzleInput) -> PuzzleResult {
-		return -2
 	}
 
 	// -------------------------------------------------------------
@@ -83,7 +81,7 @@ class Day21: PuzzleClass {
 			implementation: part2,
 			input: PuzzleInput(fromFile: "21-input"),
 			tests: [
-				//PuzzleTest(PuzzleInput(fromFile: "21-input-test"), result: 273),
+				PuzzleTest(PuzzleInput(fromFile: "21-input-test"), result: "mxmxvkd,sqjhc,fvjkl"),
 			]
 		),
 	]
