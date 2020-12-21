6d26ff86a70385e0e83c40fb1ad211b158ec9a45
apparentorder <github@neveragain.de>
2020-12-21 10:55:56 +0100

    slightly more cleanup

--- 2020/day21.swift
+++ 2020/day21.swift
@@ -5,7 +5,7 @@ class Day21: PuzzleClass {
 	}
 
 	var allFoods = [Food]()
-	var allergenIngredient = [String:String]()
+	var dangerousIngredients = [String:String]()
 
 	func filterAllergens() {
 		var allAllergens = Set(allFoods.flatMap { $0.allergens })
@@ -14,25 +14,22 @@ class Day21: PuzzleClass {
 			debug("NEXT ITERATION; allAllergens = \(allAllergens.sorted())")
 
 			for allergen in allAllergens {
-				var ingredients = Set<String>()
+				var ingredientsWithAllergen = Set<String>()
 
 				for food in allFoods where food.allergens.contains(allergen) {
-					ingredients = (ingredients.isEmpty) ?
-						Set(food.ingredients) :
-						ingredients.intersection(food.ingredients)
+					let ingredients = Set(food.ingredients).subtracting(dangerousIngredients.values)
 
-					debug("for \(allergen): remaining possible matches: \(ingredients)")
+					ingredientsWithAllergen = (ingredientsWithAllergen.isEmpty) ?
+						ingredients : ingredientsWithAllergen.intersection(ingredients)
 
-					guard ingredients.count != 1 else {
-						let match = ingredients.first!
-						debug("exact match: \(match) contains \(allergen)")
+					debug("for \(allergen): remaining possible matches: \(ingredientsWithAllergen)")
+
+					guard ingredientsWithAllergen.count != 1 else {
+						let ingredient = ingredientsWithAllergen.first!
+						debug("exact match: \(ingredient) contains \(allergen)")
 
 						allAllergens.remove(allergen)
-						allergenIngredient[allergen] = match
-
-						for i in 0..<allFoods.count {
-							allFoods[i].ingredients.removeAll { $0 == match }
-						}
+						dangerousIngredients[allergen] = ingredient
 
 						break
 					}
@@ -44,7 +41,8 @@ class Day21: PuzzleClass {
 	func part1(_ input: PuzzleInput) -> PuzzleResult {
 		parse(input)
 		filterAllergens()
-		let remainingIngredients = allFoods.flatMap { $0.ingredients }
+
+		let remainingIngredients = allFoods.flatMap { $0.ingredients }.filter { !dangerousIngredients.values.contains($0) }
 		debug("remaining: \(remainingIngredients)")
 		return remainingIngredients.count
 	}
@@ -52,7 +50,7 @@ class Day21: PuzzleClass {
 	func part2(_ input: PuzzleInput) -> PuzzleResult {
 		parse(input)
 		filterAllergens()
-		return allergenIngredient.sorted(by: { $0.key < $1.key }).map { $0.value }.joined(separator: ",")
+		return dangerousIngredients.sorted(by: { $0.key < $1.key }).map { $0.value }.joined(separator: ",")
 	}
 
 	func parse(_ input: PuzzleInput) {
