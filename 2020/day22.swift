9c35f0e3f7f389603a6801ed1a3508ced6e1f147
apparentorder <github@neveragain.de>
2020-12-22 09:33:01 +0100

    cleanup; add loop test; prepare for >2 players (incomplete)

--- 2020/day22.swift
+++ 2020/day22.swift
@@ -1,147 +1,117 @@
 class Day22: PuzzleClass {
-	func parse(_ input: PuzzleInput) -> [[Int]] {
-		var playerCards = [[Int]]()
+	func play(playerCards pc: [[Int]], recursiveCombat: Bool, depth: Int = 0) -> Int {
+		var playerCards = pc
+		let players = playerCards.enumerated().map { $0.0 }
 
-		var player = 0
-		playerCards += [[Int]()]
-
-		for line in input.lines {
-			guard !line.hasPrefix("Player 2") else {
-				playerCards += [[Int]()]
-				player += 1
-				continue
+		func debugDecks(_ extra: @autoclosure () -> String = "") {
+			#if DEBUG
+			let s = extra().isEmpty ? "" : " \(extra())"
+			players.forEach {
+				debug("player \($0) deck\(s): \(playerCards[$0])", indent: depth*4)
 			}
-
-			guard let i = Int(line) else { continue }
-			playerCards[player] += [i]
+			debug("")
+			#endif
 		}
 
-		return playerCards
-	}
-
-	func playR(playerCards pc: [[Int]], depth: Int = 0) -> Int {
-		var playerCards = pc
-		var deckHistory = [[[Int]]]() // deckHistory[player][round] = [cards]
-		deckHistory += [[[Int]]()] // player0
-		deckHistory += [[[Int]]()] // player1
-		var round = 0
-
 		debug("NEW GAME:", indent: depth*4)
-		debug("p1 deck: \(playerCards[0])", indent: depth*4)
-		debug("p2 deck: \(playerCards[1])", indent: depth*4)
+		debugDecks()
 
-		while !playerCards[0].isEmpty && !playerCards[1].isEmpty {
+		var round = 0
+		var deckHistory = players.map { _ in Set<[Int]>(minimumCapacity: 100) }
+
+		while playerCards.allSatisfy({ !$0.isEmpty }) {
 			round += 1
 
 			// Before either player deals a card, if there was a previous round in this
 			// game that had exactly the same cards in the same order in the same
 			// players' decks, the game instantly ends in a win for player 1.
-			var looped = [false, false]
-			//debug("deck history: \(deckHistory)")
-			for dhPlayer in [0, 1] {
-				for dhRound in deckHistory[dhPlayer] {
-					if dhRound == playerCards[dhPlayer] {
-						looped[dhPlayer] = true
-						debug("LOOP for player \(dhPlayer): \(playerCards[dhPlayer])", indent: depth*4)
-					}
+			for player in players {
+				guard !deckHistory[player].contains(playerCards[player]) else {
+					debug("LOOP for player \(player): \(playerCards[player])", indent: depth*4)
+					debug("Player 0 wins!", indent: depth*4)
+					return 0
 				}
+				deckHistory[player].insert(playerCards[player])
 			}
 
-			guard !looped[0] && !looped[1] else {
-				debug("LOOP for BOTH players! Player[0] wins!", indent: depth*4)
-				return 0
-			}
-
-			deckHistory[0] += [playerCards[0]]
-			deckHistory[1] += [playerCards[1]]
-
-			let drawnCards = [playerCards[0].removeFirst(), playerCards[1].removeFirst()]
 			var winner: Int
+			let drawnCards = players.map { playerCards[$0].removeFirst() }
 
-			// If both players have at least as many cards remaining in their deck as
-			// the value of the card they just drew, the winner of the round is
-			// determined by playing a new game of Recursive Combat
-			if playerCards[0].count >= drawnCards[0] && playerCards[1].count >= drawnCards[1] {
-				var recursivePlayerCards = playerCards
-				recursivePlayerCards[0].removeLast(recursivePlayerCards[0].count - drawnCards[0])
-				recursivePlayerCards[1].removeLast(recursivePlayerCards[1].count - drawnCards[1])
-				winner = playR(playerCards: recursivePlayerCards, depth: depth + 1)
+			if !recursiveCombat || !players.allSatisfy({ playerCards[$0].count >= drawnCards[$0] }) {
+				let winningCard = drawnCards.max()
+				winner = players.filter { drawnCards[$0] == winningCard }.first!
 			} else {
-				winner = (drawnCards[0] > drawnCards[1]) ? 0 : 1
+				// Part 2:
+				// If both players have at least as many cards remaining in their deck as
+				// the value of the card they just drew, the winner of the round is
+				// determined by playing a new game of Recursive Combat
+				// ...
+				// the quantity of cards copied is equal to the number on the card they
+				// drew to trigger the sub-game
+				// 🧠
+				let subGamePlayerCards = players.map {
+					Array(playerCards[$0].dropLast(playerCards[$0].count - drawnCards[$0]))
+				}
+				winner = play(playerCards: subGamePlayerCards, recursiveCombat: true, depth: depth + 1)
 			}
 
+			// XXX this is the only place that's hard-coded to two players.
+			// the rules are not clear how this would be handled for >2 players,
+			// and simply going round-robin downwards by card value would mean
+			// to re-write this function so it returns all drawn cards of a sub-game.
+			// therefore, i'll leave it as it is for now.
 			if winner == 0 {
 				playerCards[0] += [drawnCards[0], drawnCards[1]]
 			} else {
 				playerCards[1] += [drawnCards[1], drawnCards[0]]
 			}
 
-			debug("p1 deck after round \(round): \(playerCards[0])", indent: depth*4)
-			debug("p2 deck after round \(round): \(playerCards[1])", indent: depth*4)
-			debug("")
+			debugDecks("after round \(round)")
 		}
 
-		debug("p1 deck after game: \(playerCards[0])", indent: depth*4)
-		debug("p2 deck after game: \(playerCards[1])", indent: depth*4)
-		debug("")
+		debugDecks("after game")
+
+		// XXX for >2 players, we assume that only one player still
+		// holds any cards (i.e. *all* other players dropped out first)
+		// (not yet implemented)
 
 		guard depth == 0 else {
 			// this is a sub-game, return winning player id
-			return playerCards[0].isEmpty ? 1 : 0
+			return players.filter { !playerCards[$0].isEmpty }.first!
 		}
 
-		// top-level game, return score
-		let winningCards = playerCards[0].isEmpty ? playerCards[1] : playerCards[0]
-		var score = 0
-		for (i, card) in winningCards.reversed().enumerated() {
-			score += card*(i+1)
-		}
-
-		return score
-	}
-
-	func play(playerCards pc: [[Int]]) -> Int {
-		var playerCards = pc
-
-		while !playerCards[0].isEmpty && !playerCards[1].isEmpty {
-			let drawnCards = [playerCards[0].removeFirst(), playerCards[1].removeFirst()]
-
-			if drawnCards[0] > drawnCards[1] {
-				playerCards[0] += [drawnCards[0], drawnCards[1]]
-			} else {
-				playerCards[1] += [drawnCards[1], drawnCards[0]]
-			}
-
-			debug("p1 deck: \(playerCards[0])")
-			debug("p2 deck: \(playerCards[1])")
-			debug("")
-		}
-
-		debug("p1 deck: \(playerCards[0])")
-		debug("p2 deck: \(playerCards[1])")
-
-		let winningCards = playerCards[0].isEmpty ? playerCards[1] : playerCards[0]
-		var score = 0
-		for (i, card) in winningCards.reversed().enumerated() {
-			score += card*(i+1)
-		}
-		return score
+		// top-level game, return the score
+		return playerCards
+			.flatMap { $0 }
+			.reversed()
+			.enumerated()
+			.reduce(0, { $0 + $1.1 * ($1.0 + 1) })
 	}
 
 	func part1(_ input: PuzzleInput) -> PuzzleResult {
-		var playerCards = parse(input)
-		debug("\(playerCards)")
-		return play(playerCards: playerCards)
+		let playerCards = parse(input)
+		return play(playerCards: playerCards, recursiveCombat: false)
 	}
 
 	func part2(_ input: PuzzleInput) -> PuzzleResult {
-		var playerCards = parse(input)
+		let playerCards = parse(input)
+		return play(playerCards: playerCards, recursiveCombat: true)
+	}
 
-		// loop detection test
-		//playerCards = [[43, 19], [2, 29, 14]]
-		//return playR(playerCards: playerCards)
+	func parse(_ input: PuzzleInput) -> [[Int]] {
+		var playerCards = [[Int]]()
 
-		return playR(playerCards: playerCards)
+		for line in input.lines {
+			guard !line.hasPrefix("Player") else {
+				playerCards += [[Int]()]
+				continue
+			}
+
+			guard let i = Int(line) else { continue }
+			playerCards[playerCards.count - 1] += [i]
+		}
+
+		return playerCards
 	}
 
 	// -------------------------------------------------------------
@@ -159,6 +129,7 @@ class Day22: PuzzleClass {
 			input: PuzzleInput(fromFile: "22-input"),
 			tests: [
 				PuzzleTest(PuzzleInput(fromFile: "22-input-test"), result: 291),
+				PuzzleTest(PuzzleInput(fromFile: "22-input-test-loop"), result: 0),
 			]
 		),
 	]
