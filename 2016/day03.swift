b0601bb7213646cb242ec0dc57f5d22bfb10ed1a
apparentorder <github@neveragain.de>
2021-01-10 19:01:45 +0100

    day03

--- 2016/day03.swift
+++ 2016/day03.swift
@@ -1,10 +1,32 @@
 class Day03: PuzzleClass {
+	func countValidTriangles(_ intlist: [Int], isPart2: Bool) -> Int {
+		var r = 0
+
+		for i in 0..<intlist.count/3 {
+			let base = isPart2 ? 3*3*(i/3) + (i%3): i*3
+			let skew = isPart2 ? 3 : 1
+			let side1 = intlist[base + skew*0]
+			let side2 = intlist[base + skew*1]
+			let side3 = intlist[base + skew*2]
+
+			if (
+				side1 + side2 > side3 &&
+				side1 + side3 > side2 &&
+				side2 + side3 > side1
+			) {
+				r += 1
+			}
+		}
+
+		return r
+	}
+
 	func part1(_ input: PuzzleInput) -> PuzzleResult {
-		return -1
+		return countValidTriangles(input.intArray, isPart2: false)
 	}
 
 	func part2(_ input: PuzzleInput) -> PuzzleResult {
-		return -2
+		return countValidTriangles(input.intArray, isPart2: true)
 	}
 
 	// -------------------------------------------------------------
