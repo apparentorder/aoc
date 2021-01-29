9eab7823e22479bec1a4b046540e6482cbb6d277
apparentorder <github@neveragain.de>
2021-01-29 16:13:25 +0100

    day04, day05

--- 2016/day04.swift
+++ 2016/day04.swift
@@ -1,10 +1,97 @@
 class Day04: PuzzleClass {
+	struct Room {
+		let name: String
+		let sectorId: Int
+		let checksum: String
+
+		var decodedName: String {
+			var decodedName = name
+
+			for _ in 0..<sectorId {
+				var nextDecodedName = ""
+				for c in decodedName {
+					guard c != "z" else {
+						nextDecodedName += "a"
+						continue
+					}
+
+					guard c != "-" && c != " " else {
+						nextDecodedName += " "
+						continue
+					}
+
+					nextDecodedName += String(UnicodeScalar(c.asciiValue! + 1))
+				}
+
+				decodedName = nextDecodedName
+			}
+
+			return decodedName
+			// yes, this could be much faster.
+		}
+
+		init?(_ s: String) {
+			// example input string: aaaaa-bbb-z-y-x-123[abxyz]
+
+			var letterCounts = [Character:Int]()
+
+			let split = s.components(separatedBy: "[")
+			checksum = String(split[1].dropLast())
+
+			let nameSplit = split[0].components(separatedBy: "-")
+			sectorId = Int(nameSplit.last!)!
+			name = nameSplit.dropLast().joined(separator: "-")
+
+			for c in name where c != "-" {
+				letterCounts[c] = (letterCounts[c] ?? 0) + 1
+			}
+
+			var computedChecksum = ""
+			while computedChecksum.count < 5 {
+				let highestCount = letterCounts.values.max()!
+				let highestLetters = letterCounts.filter { $0.value == highestCount }.keys
+
+				let nextLetter = highestLetters.sorted().first!
+				letterCounts.removeValue(forKey: nextLetter)
+
+				computedChecksum += String(nextLetter)
+			}
+
+			debug(
+				"room \(name) sectorId \(sectorId) checksum "
+				+ "given \(checksum) checksum computed \(computedChecksum) --> "
+				+ "\(computedChecksum == checksum ? "valid" : "INVALID")"
+			)
+
+			guard computedChecksum == checksum else { return nil }
+		}
+	}
+
 	func part1(_ input: PuzzleInput) -> PuzzleResult {
-		return -1
+		var rooms = [Room]()
+
+		for line in input.lines {
+			if let r = Room(line) {
+				rooms += [r]
+			}
+		}
+
+		return rooms.map { $0.sectorId }.reduce(0, +)
 	}
 
 	func part2(_ input: PuzzleInput) -> PuzzleResult {
-		return -2
+		var rooms = [Room]()
+
+		for line in input.lines {
+			if let r = Room(line) {
+				rooms += [r]
+			}
+		}
+
+		rooms.forEach { debug("\($0.decodedName) (\($0.name))") }
+
+		// name found by manual inspection of decoded names (grep pole)
+		return rooms.filter { $0.decodedName == "northpole object storage" }.first!.sectorId
 	}
 
 	// -------------------------------------------------------------
@@ -14,7 +101,7 @@ class Day04: PuzzleClass {
 			implementation: part1,
 			input: PuzzleInput(fromFile: "04-input"),
 			tests: [
-				//PuzzleTest(PuzzleInput(fromFile: "04-test1"), result: 514579),
+				PuzzleTest(PuzzleInput(fromFile: "04-input-test"), result: 1514),
 			]
 		),
 		"p2": Puzzle(
--- 2016/day05.swift
+++ 2016/day05.swift
@@ -1,10 +1,83 @@
+import CryptoKit
+
+// with all optimizations, still still takes an excessive amount of time
+// (for part2 up to 40sec).
+// there must be "a better solution" (i'm guessing it's related to an MD5
+// prefix collision attack that I won't look up just to copy it)
+
 class Day05: PuzzleClass {
+	struct DoorCodeSequence: Sequence {
+		let doorId: String
+
+		func makeIterator() -> DoorCodeIterator {
+			return DoorCodeIterator(doorId: doorId)
+		}
+	}
+
+	struct DoorCodeIterator: IteratorProtocol {
+		let doorId: String
+		var index = 0
+
+		let twoZeroBytes = Array(repeating: UInt8(0), count: 2)
+
+		mutating func next() -> String? {
+			// UNLIMITED SEQUENCE!
+			while true {
+				index += 1
+
+				let message = doorId + String(index)
+				let digest = Insecure.MD5.hash(data: message.data(using: .utf8)!)
+
+				// we can easily skip hex values starting with "00 00" (i.e. two *bytes* of 0).
+				guard digest.starts(with: twoZeroBytes) else { continue }
+
+				// also note that we only need the first four bytes
+				let hex = digest.prefix(4).map { String(format: "%02x", $0) }
+
+				// still need to check if the fifth hex digit (third byte) also is zero.
+				guard hex[2].first! == "0" else { continue }
+
+				debug("\(message) -> \(hex.joined())")
+				return hex.joined()
+			}
+		}
+	}
+
 	func part1(_ input: PuzzleInput) -> PuzzleResult {
-		return -1
+		var password = ""
+		for hash in DoorCodeSequence(doorId: input.raw) {
+			password += String(Array(hash)[5])
+			debug("password: \(password)")
+
+			guard password.count < 8 else { return password }
+		}
+
+		return "never returns"
 	}
 
 	func part2(_ input: PuzzleInput) -> PuzzleResult {
-		return -2
+		var password = Array(repeating: Optional<Character>(nil), count: 8)
+
+		for hash in DoorCodeSequence(doorId: input.raw) {
+			let posStr = String(Array(hash)[5])
+
+			guard let pos = Int(posStr), pos >= 0 && pos <= 7 else {
+				debug("invalid pos \(posStr), skipping")
+				continue
+			}
+
+			guard password[pos] == nil else {
+				debug("pos \(pos) already known, skipping")
+				continue
+			}
+
+			password[pos] = Array(hash)[6]
+			debug("password: \(String(password.map { $0 ?? "_" }))")
+
+			guard password.contains(nil) else { return String(password.map { $0! }) }
+		}
+
+		return "never returns"
 	}
 
 	// -------------------------------------------------------------
@@ -12,16 +85,16 @@ class Day05: PuzzleClass {
 	lazy var puzzleConfig = [
 		"p1": Puzzle(
 			implementation: part1,
-			input: PuzzleInput(fromFile: "05-input"),
+			input: PuzzleInput(fromString: "ffykfhsq"),
 			tests: [
-				//PuzzleTest(PuzzleInput(fromFile: "05-test1"), result: 514579),
+				PuzzleTest(PuzzleInput(fromString: "abc"), result: "18f47a30"),
 			]
 		),
 		"p2": Puzzle(
 			implementation: part2,
-			input: PuzzleInput(fromFile: "05-input"),
+			input: PuzzleInput(fromString: "ffykfhsq"),
 			tests: [
-				//PuzzleTest(PuzzleInput(fromFile: "05-test1"), result: 241861950),
+				PuzzleTest(PuzzleInput(fromString: "abc"), result: "05ace8e3"),
 			]
 		),
 	]
