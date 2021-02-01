096f0259f0214cc35b1ae89fb2470ccdf6b29e9a
apparentorder <github@neveragain.de>
2021-02-01 16:02:01 +0100

    day06

--- 2016/day06.swift
+++ 2016/day06.swift
@@ -1,10 +1,31 @@
 class Day06: PuzzleClass {
+	func decodeRepetition(_ input: [String], useLeastCommon: Bool = false) -> String {
+		var r = Array(repeating: Character("_"), count: input[0].count)
+
+		for pos in 0..<input[0].count {
+			var letterCount = [Character:Int]()
+
+			for code in input {
+				let codeChars = Array(code)
+				letterCount[codeChars[pos]] = (letterCount[codeChars[pos]] ?? 0) + 1
+			}
+
+			let filterValue = useLeastCommon ?
+				letterCount.values.min()! :
+				letterCount.values.max()!
+
+			r[pos] = letterCount.filter { $0.value == filterValue }.first!.key
+		}
+
+		return String(r)
+	}
+
 	func part1(_ input: PuzzleInput) -> PuzzleResult {
-		return -1
+		return decodeRepetition(input.lines)
 	}
 
 	func part2(_ input: PuzzleInput) -> PuzzleResult {
-		return -2
+		return decodeRepetition(input.lines, useLeastCommon: true)
 	}
 
 	// -------------------------------------------------------------
@@ -14,14 +35,14 @@ class Day06: PuzzleClass {
 			implementation: part1,
 			input: PuzzleInput(fromFile: "06-input"),
 			tests: [
-				//PuzzleTest(PuzzleInput(fromFile: "06-test1"), result: 514579),
+				PuzzleTest(PuzzleInput(fromFile: "06-input-test"), result: "easter"),
 			]
 		),
 		"p2": Puzzle(
 			implementation: part2,
 			input: PuzzleInput(fromFile: "06-input"),
 			tests: [
-				//PuzzleTest(PuzzleInput(fromFile: "06-test1"), result: 241861950),
+				PuzzleTest(PuzzleInput(fromFile: "06-input-test"), result: "advent"),
 			]
 		),
 	]
