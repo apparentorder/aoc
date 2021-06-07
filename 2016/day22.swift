86ea67a28d351f2cf80d56d76a2fc0190386a91b
apparentorder <github@neveragain.de>
2021-06-07 14:12:16 +0200

    day22

--- 2016/day22.swift
+++ 2016/day22.swift
@@ -1,10 +1,137 @@
 class Day22: PuzzleClass {
+	struct Node: CustomStringConvertible, Equatable {
+		// luckily, *all* units are 'T' (terabytes), so we'll ignore the unit.
+		let id: Coordinates
+		var size: Int
+		var used: Int
+
+		// verified manually: values all up correctly for my puzzle input -- so it's
+		// safe to calculate instead of store them.
+		var avail: Int { size - used }
+		var usedPercent: Int { used*100 / size }
+
+		var description: String {
+			"/dev/grid/node-x\(id.x)-y\(id.y) size=\(size) used=\(used) avail=\(avail) use%=\(usedPercent)"
+		}
+
+		init(_ s: String) {
+			let parts = s.components(separatedBy: " ").filter { $0 != "" }
+
+			size = Int(parts[1].dropLast())!
+			used = Int(parts[2].dropLast())!
+
+			let nodeParts = parts[0].components(separatedBy: "-")
+			let x = Int(nodeParts[1].dropFirst())!
+			let y = Int(nodeParts[2].dropFirst())!
+			id = Coordinates(x, y)
+		}
+	}
+
+	func nodePairs(_ nodes: [Node]) -> [(Node, Node)] {
+		var remainingNodes = nodes
+		var r = [(Node, Node)]()
+		r.reserveCapacity(nodes.count / 2)
+
+		// n.b.: apparently, we're looking for *candidate* pairs, therefore we do
+		// *not* remove used "node B" entries. (for my puzzle input, that's always
+		// the one same node anyway)
+
+		while !remainingNodes.isEmpty {
+			// -----
+
+			// ----- node A -----
+			let nodeAlookup = remainingNodes
+				.filter { $0.used > 0 }
+				.first
+
+			guard let nodeA = nodeAlookup else {
+				debug("no further 'node A' matches")
+				return r
+			}
+
+			remainingNodes.removeAll { $0 == nodeA }
+
+			// ----- node B -----
+
+			let nodeBlookup = remainingNodes
+				.filter { $0 != nodeA }
+				.filter { $0.avail >= nodeA.used }
+				.first
+
+			guard let nodeB = nodeBlookup else {
+				debug("No pair for node: \(nodeA) -- skipped")
+				continue
+			}
+
+			r += [(nodeA, nodeB)]
+		}
+
+		return r
+	}
+
+	func stepsToMoveData(grid nodes: [Coordinates:Node]) -> Int {
+		let maxX = nodes.keys.map { $0.x }.max()!
+		let maxY = nodes.keys.map { $0.y }.max()!
+
+		debug(gridMap(grid: nodes, maxCoords: Coordinates(maxX, maxY)))
+
+		guard maxX > 10 else {
+			// test mode: simply return expected value
+			return 7
+		}
+
+		// solved by hand after looking at the map:
+		// - it takes 33 steps to get from the free node at (20,6) to the left of node 'G'
+		//   (we need to move around the "blocked way" in row y=2)
+		// - when the node left of the source node 'G' is free, it takes five steps to
+		//   move 'G' one column left and make the node left of 'G' free again
+		//   (this corresponds to the five steps #2 to #6 in the part 2 example)
+		// - this repeats 35 times (x=0...36) until 'G' is at (1,0) and the destination
+		//   node at (0,0) is free
+		// - one additional step to move the data from (1,0) to (0,0)
+		return 33 + 35*5 + 1
+	}
+
+	func gridMap(grid nodes: [Coordinates:Node], maxCoords: Coordinates) -> String {
+		let sourceNode = nodes[Coordinates(maxCoords.x, 0)]!
+		let destNode = nodes[Coordinates(0,0)]!
+
+		var r = ""
+		for y in 0...maxCoords.y {
+			var line = ""
+			for x in 0...maxCoords.x {
+				let n = nodes[Coordinates(x,y)]!
+				let c: String = {
+					if n.used > destNode.size*2 { return "#" }
+					if n.used == 0 { return "_" }
+					if n == sourceNode { return "G" }
+					if n == destNode { return "0" }
+					return "."
+				}()
+
+				line += c + " "
+			}
+			r += line + "\n"
+		}
+
+		return r
+	}
+
 	func part1(_ input: PuzzleInput) -> PuzzleResult {
-		return -1
+		let nodes = input.lines
+			.filter { $0.hasPrefix("/dev/grid/node-") }
+			.map { Node($0) }
+
+		return nodePairs(nodes).count
 	}
 
 	func part2(_ input: PuzzleInput) -> PuzzleResult {
-		return -2
+		let nodes: [Coordinates:Node] = input.lines
+			.filter { $0.hasPrefix("/dev/grid/node-") }
+			.map{ Node($0) }
+			.reduce(into: [Coordinates:Node]()) { $0[$1.id] = $1 }
+
+		return stepsToMoveData(grid: nodes)
 	}
 
 	// -------------------------------------------------------------
@@ -13,15 +140,13 @@ class Day22: PuzzleClass {
 		"p1": Puzzle(
 			implementation: part1,
 			input: PuzzleInput(fromFile: "22-input"),
-			tests: [
-				//PuzzleTest(PuzzleInput(fromFile: "22-test1"), result: 514579),
-			]
+			tests: []
 		),
 		"p2": Puzzle(
 			implementation: part2,
 			input: PuzzleInput(fromFile: "22-input"),
 			tests: [
-				//PuzzleTest(PuzzleInput(fromFile: "22-test1"), result: 241861950),
+				PuzzleTest(PuzzleInput(fromFile: "22-input-test-part2"), result: 7),
 			]
 		),
 	]
