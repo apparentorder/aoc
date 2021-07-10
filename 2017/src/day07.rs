cf059f2a8a229f82f9f886ccd285420d56a171e5
apparentorder <github@neveragain.de>
2021-07-10 08:48:00 +0200

    create dummies for days 03 .. 25

---
+++ 2017/src/day07.rs
@@ -0,0 +1,22 @@
+use crate::aoc;
+
+pub fn part1(input: String) -> String {
+	return input;
+}
+
+pub fn part2(input: String) -> String {
+	return input;
+}
+
+pub const PUZZLE_DATA: aoc::Puzzle = aoc::Puzzle {
+	day: 07,
+	input: "file:07-input",
+	implementation_part1: part1,
+	implementation_part2: part2,
+	tests_part1: &[
+		// ("result", "input"),
+	],
+	tests_part2: &[
+	],
+};
+
