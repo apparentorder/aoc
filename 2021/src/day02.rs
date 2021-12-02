cea0636e44e2628e144dacf5cb567d346d235cbe
apparentorder <github@neveragain.de>
2021-12-02 06:18:48 +0100

    day02 cleanup

--- 2021/src/day02.rs
+++ 2021/src/day02.rs
@@ -1,27 +1,6 @@
 use crate::aoc;
 
-pub fn part1(input: String) -> String {
-	let mut x = 0;
-	let mut y = 0;
-
-	for line in input.split('\n') {
-		let mut parts = line.split_whitespace();
-
-		let movement = parts.next().unwrap();
-		let val: i32 = parts.next().unwrap().parse().unwrap();
-
-		match movement {
-			"forward" => x += val,
-			"down" => y += val,
-			"up" => y -= val,
-			_ => panic!(),
-		}
-	}
-
-	return (x*y).to_string()
-}
-
-pub fn part2(input: String) -> String {
+fn dive(input: String, is_part2: bool) -> i32 {
 	let mut x = 0;
 	let mut y = 0;
 	let mut aim = 0;
@@ -32,18 +11,35 @@ pub fn part2(input: String) -> String {
 		let movement = parts.next().unwrap();
 		let val: i32 = parts.next().unwrap().parse().unwrap();
 
-		match movement {
-			"forward" => {
-				x += val;
-				y += aim * val;
-			},
-			"down" => aim += val,
-			"up" => aim -= val,
-			_ => panic!(),
+		if is_part2 {
+			match movement {
+				"up" => aim -= val,
+				"down" => aim += val,
+				"forward" => {
+					x += val;
+					y += aim * val;
+				},
+				_ => panic!(),
+			}
+		} else /* part1 */ {
+			match movement {
+				"up" => y -= val,
+				"down" => y += val,
+				"forward" => x += val,
+				_ => panic!(),
+			}
 		}
 	}
 
-	return (x*y).to_string()
+	return x*y
+}
+
+pub fn part1(input: String) -> String {
+	dive(input, false).to_string()
+}
+
+pub fn part2(input: String) -> String {
+	dive(input, true).to_string()
 }
 
 pub const PUZZLE_DATA: aoc::Puzzle = aoc::Puzzle {
@@ -55,6 +51,7 @@ pub const PUZZLE_DATA: aoc::Puzzle = aoc::Puzzle {
 		("150", "file:02-input-test"),
 	],
 	tests_part2: &[
+		("900", "file:02-input-test"),
 	],
 };
 
