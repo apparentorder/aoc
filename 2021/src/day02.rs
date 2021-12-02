4bdf41725d71961b88354abd9744c398058e2431
apparentorder <github@neveragain.de>
2021-12-02 06:08:21 +0100

    day02 as used

--- 2021/src/day02.rs
+++ 2021/src/day02.rs
@@ -1,13 +1,49 @@
 use crate::aoc;
 
 pub fn part1(input: String) -> String {
-	let _ = input;
-	return 0.to_string()
+	let mut x = 0;
+	let mut y = 0;
+
+	for line in input.split('\n') {
+		let mut parts = line.split_whitespace();
+
+		let movement = parts.next().unwrap();
+		let val: i32 = parts.next().unwrap().parse().unwrap();
+
+		match movement {
+			"forward" => x += val,
+			"down" => y += val,
+			"up" => y -= val,
+			_ => panic!(),
+		}
+	}
+
+	return (x*y).to_string()
 }
 
 pub fn part2(input: String) -> String {
-	let _ = input;
-	return 0.to_string()
+	let mut x = 0;
+	let mut y = 0;
+	let mut aim = 0;
+
+	for line in input.split('\n') {
+		let mut parts = line.split_whitespace();
+
+		let movement = parts.next().unwrap();
+		let val: i32 = parts.next().unwrap().parse().unwrap();
+
+		match movement {
+			"forward" => {
+				x += val;
+				y += aim * val;
+			},
+			"down" => aim += val,
+			"up" => aim -= val,
+			_ => panic!(),
+		}
+	}
+
+	return (x*y).to_string()
 }
 
 pub const PUZZLE_DATA: aoc::Puzzle = aoc::Puzzle {
@@ -16,7 +52,7 @@ pub const PUZZLE_DATA: aoc::Puzzle = aoc::Puzzle {
 	implementation_part1: part1,
 	implementation_part2: part2,
 	tests_part1: &[
-		// ("9", "file:02-input-test"),
+		("150", "file:02-input-test"),
 	],
 	tests_part2: &[
 	],
