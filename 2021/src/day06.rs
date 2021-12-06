b5a9211b2759fb24bd5503914fba89df55be0034
apparentorder <github@neveragain.de>
2021-12-06 11:30:35 +0100

    day06 with less brute force

--- 2021/src/day06.rs
+++ 2021/src/day06.rs
@@ -1,48 +1,28 @@
 use crate::aoc;
 
-fn fishs_after_days(days: i64) -> Vec<i64> {
-	// returns a vec indicating how many fishs there will be after `days` days, from a single initial fish,
-	// for each possible initial state (reflected by the vec's index)
+fn model(input: String, days: i32) -> Vec<i64> {
+	let fishs: Vec<i64> = input.split(',').map(|s| s.parse().unwrap()).collect();
+	let mut counts: Vec<i64> = (0..=8).map(|i| fishs.iter().filter(|&f| i == *f).count() as i64).collect();
 
-	let mut fishs = vec![];
-	let mut fishs_after_days = vec![];
-
-	for initial_state in 0..7 {
-		fishs.clear();
-		fishs.push(initial_state);
-
-		for _ in 0..days {
-			for i in 0..fishs.len() {
-				if fishs[i] == 0 {
-					fishs[i] = 6;
-					fishs.push(8);
-				} else {
-					fishs[i] -= 1;
-				}
-			}
+	for _ in 0..days {
+		let spawns = counts[0];
+		for i in 0..=7 {
+			counts[i] = counts[i + 1];
 		}
-
-		fishs_after_days.push(fishs.len() as i64);
-		println!("initial state {} after {} days: {}", initial_state, days, fishs.len());
+		counts[6] += spawns;
+		counts[8] = spawns;
 	}
 
-	return fishs_after_days
-}
-
-fn model(fishs: &mut Vec<i64>, days: i64) -> i64 {
-	let fish_table = fishs_after_days(days);
-
-	return fishs.iter().map(|&f| fish_table[f as usize]).sum()
+	//println!("counts {:?}", counts);
+	return counts
 }
 
 pub fn part1(input: String) -> String {
-	let mut fishs = input.split(',').map(|i| i.parse::<i64>().unwrap()).collect();
-	return model(&mut fishs, 80).to_string()
+	return model(input, 80).iter().sum::<i64>().to_string()
 }
 
 pub fn part2(input: String) -> String {
-	let mut fishs = input.split(',').map(|i| i.parse::<i64>().unwrap()).collect();
-	return model(&mut fishs, 256).to_string()
+	return model(input, 256).iter().sum::<i64>().to_string()
 }
 
 pub const PUZZLE_DATA: aoc::Puzzle = aoc::Puzzle {
