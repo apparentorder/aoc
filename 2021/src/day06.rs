3e07ed02cb88f664ca970662b83ee7e5b8f329e6
apparentorder <github@neveragain.de>
2021-12-06 18:35:38 +0100

    day06 useless optimization

--- 2021/src/day06.rs
+++ 2021/src/day06.rs
@@ -4,16 +4,33 @@ fn model(input: String, days: i32) -> Vec<i64> {
 	let fishs: Vec<i64> = input.split(',').map(|s| s.parse().unwrap()).collect();
 	let mut counts: Vec<i64> = (0..=8).map(|i| fishs.iter().filter(|&f| i == *f).count() as i64).collect();
 
-	for _ in 0..days {
+	for day in (7..=days).step_by(7) {
+		let old7 = counts[7];
+		let old8 = counts[8];
+
+		counts[7]  = counts[5];
+		counts[8]  = counts[6];
+
+		counts[6] += counts[4];
+		counts[5] += counts[3];
+		counts[4] += counts[2];
+		counts[3] += counts[1];
+		counts[2] += counts[0];
+		counts[1] += old8;
+		counts[0] += old7;
+		println!("day {} counts {:?}", day, counts);
+	}
+
+	for day in ((days/7)*7 + 1)..=days {
 		let spawns = counts[0];
 		for i in 0..=7 {
 			counts[i] = counts[i + 1];
 		}
 		counts[6] += spawns;
 		counts[8] = spawns;
+		println!("day {} counts {:?}", day, counts);
 	}
 
-	//println!("counts {:?}", counts);
 	return counts
 }
 
