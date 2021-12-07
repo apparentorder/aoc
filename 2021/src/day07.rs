12e287c0b137b157b493aeedc4a21e6b49964d38
apparentorder <github@neveragain.de>
2021-12-07 06:20:18 +0100

    day07 as used

--- 2021/src/day07.rs
+++ 2021/src/day07.rs
@@ -1,13 +1,29 @@
 use crate::aoc;
 
 pub fn part1(input: String) -> String {
-	let _ = input;
-	return 0.to_string()
+	let ints: Vec<i32> = input.split(',').map(|s| s.parse::<i32>().unwrap()).collect();
+	let mut cheapest = i32::MAX;
+
+	for target_position in &ints {
+		let fuel = ints.iter().fold(0, |sum, i| sum + (target_position - i).abs());
+
+		cheapest = cheapest.min(fuel);
+	}
+
+	return cheapest.to_string()
 }
 
 pub fn part2(input: String) -> String {
-	let _ = input;
-	return 0.to_string()
+	let ints: Vec<i32> = input.split(',').map(|s| s.parse::<i32>().unwrap()).collect();
+	let mut cheapest = i32::MAX;
+
+	for target_position in 1..=*ints.iter().max().unwrap() {
+		let fuel = ints.iter().fold(0, |sum, i| sum + (1..=((target_position - i).abs())).fold(0, |isum, i| isum + i));
+
+		cheapest = cheapest.min(fuel);
+	}
+
+	return cheapest.to_string()
 }
 
 pub const PUZZLE_DATA: aoc::Puzzle = aoc::Puzzle {
@@ -16,9 +32,10 @@ pub const PUZZLE_DATA: aoc::Puzzle = aoc::Puzzle {
 	implementation_part1: part1,
 	implementation_part2: part2,
 	tests_part1: &[
-		// ("9", "file:07-input-test"),
+		("37", "16,1,2,0,4,2,7,1,2,14"),
 	],
 	tests_part2: &[
+		("168", "16,1,2,0,4,2,7,1,2,14"),
 	],
 };
 
