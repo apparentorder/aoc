f8d98d38ab886e3fcc425105934a3c3e62cc39af
apparentorder <github@neveragain.de>
2021-12-10 07:06:31 +0100

    day10 cleanup

--- 2021/src/day10.rs
+++ 2021/src/day10.rs
@@ -1,131 +1,66 @@
 use crate::aoc;
-use std::collections::HashMap;
 
-fn parse(input: String) -> i64 {
-	let mut depth: HashMap<char, i64> = HashMap::new();
-	let mut illegals: HashMap<char, i64> = HashMap::new();
-	let mut stack: Vec<char> = vec![];
-
-	for line in input.lines() {
-		for char in line.chars() {
-			match char {
-				'(' | '[' | '{' | '<' => {
-					depth.insert(char, depth.get(&char).unwrap_or(&0) + 1);
-					stack.push(char);
-				},
-				')' | ']' | '}' | '>' => {
-					if stack.is_empty() {
-						break; // ok for now
-					}
-
-					let expected_char = match stack.iter().last().unwrap() {
-						'(' => ')',
-						'[' => ']',
-						'{' => '}',
-						'<' => '>',
-						_ => panic!(),
-					};
-
-					if expected_char != char {
-						illegals.insert(char, illegals.get(&char).unwrap_or(&0) + 1);
-						//println!("line {} char {} stack {:?}", line, char, stack);
-						break;
-					}
-					depth.insert(char, depth.get(&char).unwrap_or(&0) - 1);
-					stack.remove(stack.len() - 1);
-				},
-				_ => panic!(),
-			}
-		}
-	}
-
-	let mut score = 0;
-	score += illegals.get(&')').unwrap_or(&0) * 3;
-	score += illegals.get(&']').unwrap_or(&0) * 57;
-	score += illegals.get(&'}').unwrap_or(&0) * 1197;
-	score += illegals.get(&'>').unwrap_or(&0) * 25137;
-
-	return score
-}
-
-fn parse2(input: String) -> i64 {
-	let mut depth: HashMap<char, i64> = HashMap::new();
-	let mut stack: Vec<char> = vec![];
-	let mut scores: Vec<i64> = vec![];
+fn parse(input: String) -> (i64, i64) {
+	let mut score_illegal: i64 = 0;
+	let mut scores_closing: Vec<i64> = vec![];
 
 	'line: for line in input.lines() {
-		stack.clear();
+		let mut stack: Vec<char> = vec![];
 
 		for char in line.chars() {
 			match char {
-				'(' | '[' | '{' | '<' => {
-					depth.insert(char, depth.get(&char).unwrap_or(&0) + 1);
-					stack.push(char);
-				},
+				'(' | '[' | '{' | '<' => stack.push(char),
 				')' | ']' | '}' | '>' => {
-					let expected_char = match stack.iter().last().unwrap() {
-						'(' => ')',
-						'[' => ']',
-						'{' => '}',
-						'<' => '>',
-						_ => panic!(),
-					};
+					let expected_char = closing_char(*stack.iter().last().unwrap());
 
 					if expected_char != char {
-						// skip broken lines
-						continue 'line
+						score_illegal += char_score(char, true);
+						continue 'line // skip broken lines
 					}
-					depth.insert(char, depth.get(&char).unwrap_or(&0) - 1);
 					stack.remove(stack.len() - 1);
 				},
 				_ => panic!(),
 			}
 		}
 
-		if !stack.is_empty() {
-			stack.reverse();
-			let mut closing: Vec<char> = vec![];
-			for c in &stack {
-				let closing_char = match c {
-					'(' => ')',
-					'[' => ']',
-					'{' => '}',
-					'<' => '>',
-					_ => panic!(),
-				};
+		stack.reverse();
 
-				closing.push(closing_char);
-			}
-
-			println!("line {} closing {:?}", line, closing);
-
-			let mut score = 0;
-			for c in closing {
-				score *= 5;
-				score += match c {
-					')' => 1,
-					']' => 2,
-					'}' => 3,
-					'>' => 4,
-					_ => panic!(),
-				};
-			}
-
-			println!("score {}", score);
-			scores.push(score);
-		}
+		let score = stack.iter().fold(0, |score, &c| score*5 + char_score(closing_char(c), false));
+		scores_closing.push(score);
 	}
 
-	scores.sort();
-	return scores[scores.len() / 2]
+	scores_closing.sort();
+	return (score_illegal, scores_closing[scores_closing.len() / 2])
+}
+
+fn closing_char(opening: char) -> char {
+	match opening {
+		'(' => ')',
+		'[' => ']',
+		'{' => '}',
+		'<' => '>',
+		_ => panic!(),
+	}
+}
+
+fn char_score(c: char, illegal: bool) -> i64 {
+	match c {
+		')' => if illegal { 3     } else { 1 },
+		']' => if illegal { 57    } else { 2 },
+		'}' => if illegal { 1197  } else { 3 },
+		'>' => if illegal { 25137 } else { 4 },
+		_ => panic!(),
+	}
 }
 
 pub fn part1(input: String) -> String {
-	return parse(input).to_string()
+	let (r, _) = parse(input);
+	return r.to_string()
 }
 
 pub fn part2(input: String) -> String {
-	return parse2(input).to_string()
+	let (_, r) = parse(input);
+	return r.to_string()
 }
 
 pub const PUZZLE_DATA: aoc::Puzzle = aoc::Puzzle {
