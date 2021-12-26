9b4a10813cbb2bc046322dff68a3e9ebd8b6c0fc
apparentorder <github@neveragain.de>
2021-12-26 09:51:59 +0100

    cleanup / redo as Vec instead of HashMap

--- 2021/src/day25.rs
+++ 2021/src/day25.rs
@@ -1,101 +1,60 @@
 use crate::aoc;
 use std::collections::HashMap;
 
-type Coord = (i32, i32);
-type CucumberMap = HashMap<Coord, Direction>;
+type Direction = char;
+type CucumberMap = Vec<Vec<Direction>>;
 
 struct SeaFloor {
-	x_max: i32,
-	y_max: i32,
+	x_max: usize,
+	y_max: usize,
 	cucumbers: CucumberMap,
-}
-
-#[derive(Clone, PartialEq, Copy)]
-enum Direction {
-	East,
-	South,
-}
-
-impl Direction {
-	fn from_char(c: char) -> Option<Direction> {
-		match c {
-			'>' => Some(Direction::East),
-			'v' => Some(Direction::South),
-			_ => None
-		}
-	}
-
-	fn to_char(&self) -> char {
-		match self {
-			Direction::East => '>',
-			Direction::South => 'v',
-		}
-	}
 }
 
 impl SeaFloor {
 	fn from_str(input: &str) -> SeaFloor {
-		let mut map = CucumberMap::new();
 		let x_max = input.lines().nth(0).unwrap().len() - 1;
 		let y_max = input.lines().count() - 1;
-
-		for (y, line) in input.lines().enumerate() {
-			for (x, c) in line.chars().enumerate() {
-				if let Some(dir) = Direction::from_char(c) {
-					map.insert((x as i32, y as i32), dir);
-				}
-			}
-		}
-
-		return SeaFloor {
-			x_max: x_max as i32,
-			y_max: y_max as i32,
-			cucumbers: map,
-		}
+		let cucumbers = input.lines().map(|line| line.chars().collect()).collect();
+		return SeaFloor { x_max, y_max, cucumbers }
 	}
 
 	fn move_cucumbers(&mut self, dir: Direction) -> i32 {
 		let mut new_map = self.cucumbers.clone();
 		let mut movements = 0;
 
-		let cucus = self.cucumbers.iter().filter(|&c| c.1 == &dir);
-		for (&pos, &dir) in cucus {
-			let next_pos = self.next_pos(pos, dir);
-			if !self.cucumbers.contains_key(&next_pos) {
-				movements += 1;
-				new_map.remove(&pos);
-				new_map.insert(next_pos, dir);
+		for (y, row) in self.cucumbers.iter().enumerate() {
+			for (x, &c) in row.iter().enumerate() {
+				if c == dir {
+					let (next_x, next_y) = match c {
+						'>'  => ((x + 1) % (self.x_max + 1), y),
+						'v' => (x, (y + 1) % (self.y_max + 1)),
+						_ => panic!(),
+					};
+
+					if self.cucumbers[next_y][next_x] == '.' {
+						movements += 1;
+						new_map[y][x] = '.';
+						new_map[next_y][next_x] = c;
+					}
+				}
 			}
 		}
 
 		self.cucumbers = new_map;
 		return movements
 	}
-
-	fn pos_char(&self, pos: Coord) -> char {
-		if let Some(dir) = self.cucumbers.get(&pos) {
-			return dir.to_char()
-		}
-		return '.'
-	}
-
-	fn next_pos(&self, pos: Coord, dir: Direction) -> Coord {
-		match dir {
-			Direction::East  => ((pos.0 + 1) % (self.x_max + 1), pos.1),
-			Direction::South => (pos.0, (pos.1 + 1) % (self.y_max + 1)),
-		}
-	}
 }
 
 impl std::fmt::Debug for SeaFloor {
 	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-		let mut s = String::new();
+		let mut s = self
+			.cucumbers
+			.iter()
+			.map(|row| row.iter().collect::<String>())
+			.collect::<Vec<_>>()
+			.join("\n");
 
-		for y in 0..=self.y_max {
-			s.push_str(&(0..=self.x_max).map(|x| self.pos_char((x,y))).collect::<String>());
-			s.push_str(&"\n");
-		}
-
+		s.push_str("\n");
 		return write!(f, "{}", s)
 	}
 }
@@ -106,8 +65,8 @@ pub fn part1(input: String) -> String {
 
 	loop {
 		let mut movements = 0;
-		movements += floor.move_cucumbers(Direction::East);
-		movements += floor.move_cucumbers(Direction::South);
+		movements += floor.move_cucumbers('>');
+		movements += floor.move_cucumbers('v');
 		steps += 1;
 
 		//println!("after {}", steps);
