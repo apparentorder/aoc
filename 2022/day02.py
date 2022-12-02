631c9514afa82fdededf5bdc7918f4ffa3e8ebe1
apparentorder <github@neveragain.de>
2022-12-02 07:27:21 +0100

    day02

---
+++ 2022/day02.py
@@ -0,0 +1,81 @@
+from tools.aoc import AOCDay
+from typing import Any
+
+class RPS():
+	def __init__(self, letter):
+		match letter:
+			case 'A' | 'X':
+				self.type = "Rock"
+				self.score = 1
+			case 'B' | 'Y':
+				self.type = "Paper"
+				self.score = 2
+			case 'C' | 'Z':
+				self.type = "Scissors"
+				self.score = 3
+			case _:
+				raise Exception("invalid letter: " + letter)
+
+	def against(self, opponent):
+		# returns score of game
+		if self.type == opponent.type:
+			return 3
+
+		if self.type == "Rock" and opponent.type == "Scissors":
+			return 6
+
+		if self.type == "Scissors" and opponent.type == "Paper":
+			return 6
+
+		if self.type == "Paper" and opponent.type == "Rock":
+			return 6
+
+		return 0
+
+	def __repr__(self):
+		return "%s@%d" % (self.type, self.score)
+
+def game(strategy, is_part2):
+	score = 0
+
+	for choices in strategy:
+		opponent_shape = RPS(choices[0])
+		self_shape = RPS(choices[1])
+
+		if is_part2:
+			expected_score = 0 if choices[1] == "X" else 3 if choices[1] == "Y" else 6
+
+			for try_shape in ["A", "B", "C"]:
+				self_shape = RPS(try_shape)
+				if self_shape.against(opponent_shape) == expected_score:
+					break
+
+		outcome = self_shape.against(opponent_shape)
+		score += outcome
+		score += self_shape.score
+
+		#print("Opponent=%s vs. Self=%s: outcome %d" % (opponent_shape, self_shape, outcome))
+
+	return score
+
+def parse(input):
+	return map(lambda x: x.split(), input)
+
+class Day(AOCDay):
+	inputs = [
+		[
+			(15, '02-test'),
+			(13_809, '02')
+		],
+		[
+			(12, '02-test'),
+			(12_316, '02')
+		]
+	]
+
+	def part1(self) -> Any:
+		return game(parse(self.input), False)
+
+	def part2(self) -> Any:
+		return game(parse(self.input), True)
+
