43c560271bb141056fde0356d973d354c61cfbbe
apparentorder <github@neveragain.de>
2022-12-05 07:15:13 +0100

    proper parsing

--- 2022/day05.py
+++ 2022/day05.py
@@ -2,33 +2,34 @@ from tools.aoc import AOCDay
 from typing import Any
 
 def parse(input):
-	stacks = [[] for _ in range(10)]
+	end_of_map = input.index("")
+	input_map = input[:end_of_map]
+	input_instructions = input[end_of_map + 1:]
 
-	for i in range(len(input)):
-		line = input[0]
-		input = input[1:]
+	stacks = {}
+	for i, c in enumerate(input_map.pop()):
+		if c.isnumeric():
+			stack_id = int(c)
+			stacks[stack_id] = {
+				"column": i,
+				"crates": []
+			}
 
-		if '1' in line:
-			break
-
-		num_crates = (len(line) - 2) // 4 + 1
-
-		for crate_pos in range(num_crates):
-			crate = line[1 + crate_pos*4]
+	for line in input_map:
+		for stack_id in stacks:
+			crate = line[stacks[stack_id]["column"]]
 			if crate.isupper():
-				stacks[crate_pos] += [crate]
-
-	input = input[1:] # drop empty line
+				stacks[stack_id]["crates"] += [crate]
 
 	instructions = []
-	for line in input:
-		elem = line.split(" ")
+	for line in input_instructions:
+		elem = line.split()
 		instructions += [{
 			"from_stack": int(elem[3]),
 			"to_stack": int(elem[5]),
 			"count": int(elem[1])
 		}]
-	
+
 	#print("stacks %s\ninstructions %s" % (stacks, instructions))
 	return stacks, instructions
 
@@ -37,8 +38,8 @@ def moves(stacks, instructions, multiple: bool):
 		#print(move)
 		for i in range(move["count"]):
 			target_pos = i if multiple else 0
-			crate = stacks[move["from_stack"] - 1].pop(0)
-			stacks[move["to_stack"] - 1].insert(target_pos, crate)
+			crate = stacks[move["from_stack"]]["crates"].pop(0)
+			stacks[move["to_stack"]]["crates"].insert(target_pos, crate)
 		#print(stacks)
 		#print()
 
@@ -57,12 +58,10 @@ class Day(AOCDay):
 	def part1(self) -> Any:
 		stacks, instructions = parse(self.getInput())
 		moves(stacks, instructions, False)
-		message = "".join([stack[0] for stack in stacks if len(stack) > 0])
-		return message
+		return "".join(stacks[stack_id]["crates"][0] for stack_id in sorted(stacks))
 
 	def part2(self) -> Any:
 		stacks, instructions = parse(self.getInput())
 		moves(stacks, instructions, True)
-		message = "".join([stack[0] for stack in stacks if len(stack) > 0])
-		return message
+		return "".join(stacks[stack_id]["crates"][0] for stack_id in sorted(stacks))
 
