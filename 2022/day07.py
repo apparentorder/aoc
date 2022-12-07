3cdb51d32e087b857afa7c01ce1615f074750ef6
apparentorder <github@neveragain.de>
2022-12-07 07:03:22 +0100

    day07 cleanup

--- 2022/day07.py
+++ 2022/day07.py
@@ -8,7 +8,6 @@ def parse(input):
 	for line in input:
 		elem = line.split()
 
-		#print("cwd %s\nline %s\n" % (cwd, line))
 		if elem[0] == "$":
 			if elem[1] == "cd":
 				if elem[2] == "..":
@@ -18,44 +17,39 @@ def parse(input):
 				else:
 					cwd += [elem[2]]
 
-		elif elem[0].isnumeric():
-			# file
-			size = int(elem[0])
+		# files always start with the size
+		if not elem[0].isnumeric():
+			continue
 
-			for i in range(len(cwd) + 1):
-				cwd_string = "/".join(cwd[:i])
-				s = dir_size.get(cwd_string, 0)
-				dir_size[cwd_string] = size + s
+		size = int(elem[0])
+		# update/set size for this path and all parent paths
+		for i in range(len(cwd) + 1):
+			cwd_string = "/" + "/".join(cwd[:i])
+			dir_size[cwd_string] = size + dir_size.get(cwd_string, 0)
 
 	return dir_size
 
 class Day(AOCDay):
 	inputs = [
 		[
-			(95437, '07-test')
-			,(1844187, '07')
+			(95_437, '07-test')
+			,(1_844_187, '07')
 		],
 		[
-			(24933642, '07-test')
-			,(4978279, '07')
+			(24_933_642, '07-test')
+			,(4_978_279, '07')
 		]
 	]
 
 	def part1(self) -> Any:
 		ds = parse(self.getInput())
-		return sum(s for (dir, s) in ds.items() if s <= 100000)
+		return sum(size for dir, size in ds.items() if size <= 100_000)
 
 	def part2(self) -> Any:
 		ds = parse(self.getInput())
 
-		total_size = 70000000
-		free = 70000000 - ds['']
-		needed = 30000000
+		free = 70_000_000 - ds['/']
+		needed = 30_000_000
 
-		candidates = []
-		for dir, s in ds.items():
-			if free + s > needed:
-				candidates += [s]
-
-		return min(candidates)
+		return min(size for dir, size in ds.items() if free+size > needed)
 
