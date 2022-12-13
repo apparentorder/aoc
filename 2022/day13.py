4915e5b2fd5d479b8256882488e5cb7a746de3c5
apparentorder <github@neveragain.de>
2022-12-13 08:49:42 +0100

    day13 cleanup

--- 2022/day13.py
+++ 2022/day13.py
@@ -7,30 +7,17 @@ ORDER_RIGHT = 1
 ORDER_WRONG = -1
 ORDER_UNKNOWN = 0
 
-def parse(input, group_in_pairs: bool):
-	pairs = []
+def parse(input):
+	return [json.loads(line) for line in input if line != ""]
 
-	while len(input) > 0:
-		s1 = input.pop(0)
-		if s1 == "": continue
-		pairs += [[json.loads(s1), json.loads(input.pop(0))]]
-
-	if group_in_pairs:
-		return pairs
-	else:
-		packets = [p[0] for p in pairs]
-		packets += [p[1] for p in pairs]
-		return packets
-
-def right_order(pair_list):
+def right_order(packets):
 	sum_right_index = 0
 
-	# the first value is called left and the second value is called right
-	for index, (left, right) in enumerate(pair_list):
+	for i in range(len(packets) // 2):
 		#print(f">>> Compare {left} vs {right}")
-		if packet_pair_order(left, right) == ORDER_RIGHT:
+		if packet_pair_order(packets[i*2], packets[i*2+1]) == ORDER_RIGHT:
 			#print("... right order!")
-			sum_right_index += index + 1
+			sum_right_index += i + 1
 
 	return sum_right_index
 
@@ -53,35 +40,30 @@ def packet_pair_order(left, right, depth = 0):
 	# convert the integer to a list which contains that integer as its only value
 	if type(left) is int:
 		left = [left]
-		return packet_pair_order(left, right)
 	elif type(right) is int:
 		right = [right]
-		return packet_pair_order(left, right)
 
 	# if control reaches here, both values are lists
-	assert(type(left) is list and type(right) is list)
+	#assert(type(left) is list and type(right) is list)
 
-	# we're gonna pop() 'em, so make copies.
-	left = left.copy()
-	right = right.copy()
-
-	while True:
-		# If the left list runs out of items first, the inputs are in the right order.
-		if 0 == len(left) < len(right):
-			return ORDER_RIGHT
-
-		# If the right list runs out of items first, the inputs are not in the right order
-		if 0 == len(right) < len(left):
-			return ORDER_WRONG
-
-		# If the lists are the same length and no comparison makes a decision about the order,
-		# continue checking the next part of the input.
-		if 0 == len(left) == len(right):
-			return ORDER_UNKNOWN
-
-		po = packet_pair_order(left.pop(0), right.pop(0))
+	for i in range(min([len(left), len(right)])):
+		po = packet_pair_order(left[i], right[i])
 		if po != ORDER_UNKNOWN:
 			return po
+
+	# If the lists are the same length and no comparison makes a decision about the order,
+	# continue checking the next part of the input.
+	if len(left) == len(right):
+		return ORDER_UNKNOWN
+
+	# if control reaches here, no value decided the outcome before one list was empty
+
+	# If the left list runs out of items first, the inputs are in the right order.
+	if len(left) < len(right):
+		return ORDER_RIGHT
+	else:
+		# If the right list runs out of items first, the inputs are not in the right order
+		return ORDER_WRONG
 
 class Day(AOCDay):
 	inputs = [
@@ -96,19 +78,17 @@ class Day(AOCDay):
 	]
 
 	def part1(self) -> Any:
-		pairs = parse(self.getInput(), True)
-		return right_order(pairs)
+		packets = parse(self.getInput())
+		return right_order(packets)
 
 	def part2(self) -> Any:
-		packets = parse(self.getInput(), False)
+		packets = parse(self.getInput())
 
 		div1 = [[2]]
 		div2 = [[6]]
+		packets.extend([div1, div2])
 
-		packets += [div1]
-		packets += [div2]
-
-		packets = sorted(packets, key = cmp_to_key(packet_pair_order), reverse = True)
+		packets.sort(key = cmp_to_key(packet_pair_order), reverse = True)
 		#print(packets)
 
 		r  = packets.index(div1) + 1
