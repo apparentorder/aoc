81faade2085ae90b7d897ee04b981c288433ed4b
apparentorder <apparentorder@neveragain.de>
2023-12-05 07:59:57 +0000

    day05

--- 2023/day05.py
+++ 2023/day05.py
@@ -28,6 +28,23 @@ class Mapper:
         # when no mapping matches, return input unchanged
         return input
                 
+    def distance_to_next(self, input):
+        # distance to next
+        # - when input falls within a range, return range end + 1
+        # - when input is not within a range, return the next (higher) source range's start
+        # - fallback if there is no match and no next range, return 1
+        
+        next_range_start = None
+        for m in self.mappings:
+            if m.source_range_start > input:
+                next_range_start = min(next_range_start or 2**31, m.source_range_start)
+                
+            if input in range(m.source_range_start, m.source_range_start + m.range_length):
+                dest = m.dest_range_start + input - m.source_range_start        
+                return m.dest_range_start + m.range_length - dest
+                
+        return next_range_start or 1
+        
 class Almanac:
     def __init__(self, input):
         self._mappers = []
@@ -48,10 +65,14 @@ class Almanac:
             
     def map_seed(self, seed):
         mapped_value = seed
+        distance_to_next = None
         for m in self._mappers:
+            source_value = mapped_value
             mapped_value = m.map(mapped_value)
+            distance_to_next = min(distance_to_next or 2**31, m.distance_to_next(source_value))
+            #print(f"{m.source}:{source_value} -> {m.dest}:{mapped_value} (dtn={distance_to_next})")
         
-        return mapped_value
+        return mapped_value, (distance_to_next or 1)
         
 class Day(AOCDay):
     inputs = [
@@ -61,13 +82,13 @@ class Day(AOCDay):
         ],
         [
             (46, "input5-test"),
-            (None, "input5"),
+            (69323688, "input5"),
         ]
     ]
 
     def part1(self) -> Any:
         almanac = Almanac(self.getInput())
-        locations = map(almanac.map_seed, almanac.seeds)
+        locations = map(lambda seed: (almanac.map_seed(seed))[0], almanac.seeds)
         return min(locations)
 
     def part2(self) -> Any:
@@ -78,13 +99,28 @@ class Day(AOCDay):
         while len(seed_data) > 0:
             seed_range_start = seed_data.pop(0)
             seed_range_length = seed_data.pop(0)
-            
-            for seed in range(seed_range_start, seed_range_start + seed_range_length):
-                locations += [almanac.map_seed(seed)]
-                
+
+            seed = seed_range_start
+            dtn1_count = 0
+            while seed < (seed_range_start + seed_range_length):
+                location, distance_to_next = almanac.map_seed(seed)
+                #print(f"seed {seed} -> location {location} distance_to_next {distance_to_next}")
+                locations += [location]
+                seed += distance_to_next
+
+                if distance_to_next == 1:
+                    dtn1_count += 1
+                    if dtn1_count > 100:
+                        # FIXME: it seems obvious that there is a way to determine
+                        # if continuing here makes sense or not, but I can't see it
+                        # yet. some repeated steps of 1 are required for the test
+                        # input, but don't seem to happen at all in the real input.
+                        # arbirarily abort after too many attempts here.
+                        print(f"abort at {seed}")
+                        break
+                    
         return min(locations)
-
-
+        
 if __name__ == '__main__':
     day = Day(2023, 5)
     day.run(verbose=True)
