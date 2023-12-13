6bb557f37b934429f144e78dcc412e680c345d31
apparentorder <apparentorder@neveragain.de>
2023-12-13 10:19:25 +0000

    day13

---
+++ 2023/day13.py
@@ -0,0 +1,100 @@
+from tools.aoc import AOCDay
+from typing import Any
+    
+def mirror_value_smudged(mirror):
+    v_orig = mirror_value(mirror)
+        
+    for m in generate_smudges(mirror):
+        v = mirror_value(m, except_for = v_orig)
+        if v != 0:
+            return v
+    
+    print(mirror)
+    raise Exception("bad luck")        
+            
+def mirror_value(mirror, except_for = None):
+    v = mirror_lines(mirror, "left", except_for = except_for)
+    
+    if v == 0:
+        v = mirror_lines(mirror, "above", except_for = except_for)
+
+    return v
+            
+def mirror_lines(mirror, where, except_for = None):
+    if where == "above":
+        m_max = len(mirror) - 1
+        get_line_xy = lambda v: get_line(mirror, y = v)
+    else: # "left"
+        m_max = len(mirror[0]) - 1
+        get_line_xy = lambda v: get_line(mirror, x = v)
+        
+    for v in range(m_max):
+        line = get_line_xy(v)
+       
+        if line == get_line_xy(v + 1):
+            # have match, check other lines
+            
+            for v_after in range(v + 2, min(m_max, v + 1 + v) + 1):
+                v_before = v + 1 - (v_after - v)
+                if get_line_xy(v_before) != get_line_xy(v_after):
+                    break
+            else:
+                res = (v + 1) * (100 if where == "above" else 1)
+                if not except_for or except_for != res:
+                    return res
+            
+    return 0
+    
+def generate_smudges(mirror):
+    for y in range(len(mirror)):
+        for x in range(len(mirror[0])):
+            m = mirror.copy()
+            m[y] = m[y][0:x] + ("#" if m[y][x] == "." else ".") + m[y][x+1:]
+            yield m
+            
+def get_line(mirror, x = None, y = None):
+    if x is not None:
+        return str([m[x] for m in mirror])
+    else:
+        return mirror[y]    
+    
+def parse(input):
+    buf = []
+    mirrors = []
+    
+    for line in input:
+        if line == "":
+            mirrors += [buf]
+            buf = []
+        else:
+            buf += [line]
+            
+    if len(buf) > 0:
+        mirrors += [buf]
+    
+    return mirrors
+    
+class Day(AOCDay):
+    inputs = [
+        [
+            (405, "input13-test"),
+            (30158, "input13"),
+        ],
+        [
+            (400, "input13-test"),
+            (36474, "input13"),
+        ]
+    ]
+
+    def part1(self) -> Any:
+        mirrors = parse(self.getInput())
+        return sum(mirror_value(m) for m in mirrors)
+
+    def part2(self) -> Any:
+        mirrors = parse(self.getInput())
+        return sum(mirror_value_smudged(m) for m in mirrors)
+
+
+if __name__ == '__main__':
+    day = Day(2023, 13)
+    day.run(verbose=True)
