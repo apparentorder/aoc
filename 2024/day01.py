fd7f5a8a310f839fa8ab858687906df8ad6f4e96
apparentorder <github@neveragain.de>
2024-11-30 12:35:05 +0100

    indent properly

--- 2024/day01.py
+++ 2024/day01.py
@@ -2,26 +2,26 @@ from tools.aoc import AOCDay
 from typing import Any
 
 def foo():
-    return 23
+	return 23
 
 class Day(AOCDay):
-    inputs = [
-        [
-            (142, "input1-test"),
-            (None, "input1"),
-        ],
-        [
-            (281, "input1-testp2"),
-            (None, "input1"),
-        ]
-    ]
+	inputs = [
+		[
+			(142, "input1-test"),
+			(None, "input1"),
+		],
+		[
+			(281, "input1-testp2"),
+			(None, "input1"),
+		]
+	]
 
-    def part1(self) -> Any:
-        return foo()
+	def part1(self) -> Any:
+		return foo()
 
-    def part2(self) -> Any:
-        return foo()
+	def part2(self) -> Any:
+		return foo()
 
 if __name__ == '__main__':
-    day = Day(2024, 1)
-    day.run(verbose=True)
+	day = Day(2024, 1)
+	day.run(verbose=True)
