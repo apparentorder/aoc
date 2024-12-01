089c72402fa4e4fb6beba45050a33f789262f162
apparentorder <github@neveragain.de>
2024-12-01 08:54:34 +0100

    solve day01

--- 2024/day01.py
+++ 2024/day01.py
@@ -1,26 +1,30 @@
 from tools.aoc import AOCDay
 from typing import Any
 
-def foo():
-	return "foo"
+def parse(input: [str]):
+	l0 = sorted(int(line.split()[0]) for line in input)
+	l1 = sorted(int(line.split()[1]) for line in input)
+	return (l0, l1)
 
 class Day(AOCDay):
 	inputs = [
 		[
-			(142, "input1-test"),
-			(None, "input1"),
+			(11, "input1-test"),
+			(2378066, "input1"),
 		],
 		[
-			(281, "input1-testp2"),
-			(None, "input1"),
+			(31, "input1-test"),
+			(18934359, "input1"),
 		]
 	]
 
 	def part1(self) -> Any:
-		return foo()
+		(l0, l1) = parse(self.getInput())
+		return sum(abs(i0 - l1[i]) for i, i0 in enumerate(l0))
 
 	def part2(self) -> Any:
-		return foo()
+		(l0, l1) = parse(self.getInput())
+		return sum(i0 * l1.count(i0) for i, i0 in enumerate(l0))
 
 if __name__ == '__main__':
 	day = Day(2024, 1)
